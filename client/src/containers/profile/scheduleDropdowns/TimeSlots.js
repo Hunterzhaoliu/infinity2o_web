0804fc20a186a386fd92de13c2eac83a893bb007
Q64 <quinnliu@vt.edu>
2018-02-12 17:05:30 -0600

    y

--- client/src/containers/profile/scheduleDropdowns/TimeSlots.js
+++ client/src/containers/profile/scheduleDropdowns/TimeSlots.js
@@ -1,16 +1,43 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { reduxForm } from 'redux-form';
+//import { reduxForm } from 'redux-form';
 import { Row, Col, Menu, Icon, Checkbox } from 'antd';
 //const CheckboxGroup = Checkbox.Group;
 
 class TimeSlots extends Component {
+	onChange(checkedValues) {
+		console.log('checked = ', checkedValues);
+	}
+
 	renderMenuItems(timeSlots) {
 		return _.map(timeSlots, timeSlot => {
 			console.log('timeSlot = ', timeSlot);
 			//return <div key={timeSlot.key}>Hi</div>;
-			return <Menu.Item key={timeSlot.key}>{timeSlot.label}</Menu.Item>;
+			return (
+				<Menu.Item key={timeSlot.key}>
+					{timeSlot.label}
+					<Checkbox.Group onChange={this.onChange}>
+						<Row>
+							<Col span={8}>
+								<Checkbox value="A">A</Checkbox>
+							</Col>
+							<Col span={8}>
+								<Checkbox value="B">B</Checkbox>
+							</Col>
+							<Col span={8}>
+								<Checkbox value="C">C</Checkbox>
+							</Col>
+							<Col span={8}>
+								<Checkbox value="D">D</Checkbox>
+							</Col>
+							<Col span={8}>
+								<Checkbox value="E">E</Checkbox>
+							</Col>
+						</Row>
+					</Checkbox.Group>
+				</Menu.Item>
+			);
 		});
 	}
 
@@ -40,8 +67,10 @@ function mapStateToProps(state) {
 	};
 }
 
-TimeSlots = connect(mapStateToProps, null)(TimeSlots);
+export default connect(mapStateToProps, null)(TimeSlots);
 
-export default reduxForm({
-	form: 'profileDayDropdownTimeSlots' // state.form.profileDayDropdownTimeSlots
-})(TimeSlots);
+// TimeSlots = connect(mapStateToProps, null)(TimeSlots);
+//
+// export default reduxForm({
+// 	form: 'profileDayDropdownTimeSlots' // state.form.profileDayDropdownTimeSlots
+// })(TimeSlots);
