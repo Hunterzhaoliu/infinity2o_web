6afd787b0b99e1ab1ee826501a80d4dd73540192
Q64 <quinnliu@vt.edu>
2018-02-22 19:11:24 -0600

    y

--- client/src/reducers/colorThemeReducer.js
+++ client/src/reducers/colorThemeReducer.js
@@ -66,37 +66,42 @@ function _getStateForSection(newState) {
 	return newState;
 }
 
+function _updateColorTheme(newState, action) {
+	newState.backgroundColor = action.backgroundColor;
+	newState.keyBackgroundColor = action.keyBackgroundColor;
+	newState.keyCompliment1BackgroundColor =
+		action.keyCompliment1BackgroundColor;
+	newState.keyCompliment2BackgroundColor =
+		action.keyCompliment2BackgroundColor;
+	newState.key = action.key;
+	newState.keyCompliment1 = action.keyCompliment1;
+	newState.keyCompliment2 = action.keyCompliment2;
+	newState.thirdColor = action.thirdColor;
+	newState.text1Color = action.text1Color;
+	newState.text2Color = action.text2Color;
+	newState.text3Color = action.text3Color;
+	newState.text4Color = action.text4Color;
+	newState.text5Color = action.text5Color;
+	newState.text6Color = action.text6Color;
+	newState.text7Color = action.text7Color;
+	newState.text8Color = action.text8Color;
+	newState.keyText1Color = action.keyText1Color;
+	newState.keyText2Color = action.keyText2Color;
+	newState.keyText3Color = action.keyText3Color;
+	newState.keyText4Color = action.keyText4Color;
+	newState.keyText5Color = action.keyText5Color;
+	newState.keyText6Color = action.keyText6Color;
+	newState.keyText7Color = action.keyText7Color;
+	newState.keyText8Color = action.keyText8Color;
+	newState = _getStateForSection(newState);
+	return newState;
+}
+
 export default function(state = initialState, action) {
 	let newState = cloneObject(state);
 	switch (action.type) {
 		case UPDATE_COLOR_THEME:
-			newState.backgroundColor = action.backgroundColor;
-			newState.keyBackgroundColor = action.keyBackgroundColor;
-			newState.keyCompliment1BackgroundColor =
-				action.keyCompliment1BackgroundColor;
-			newState.keyCompliment2BackgroundColor =
-				action.keyCompliment2BackgroundColor;
-			newState.key = action.key;
-			newState.keyCompliment1 = action.keyCompliment1;
-			newState.keyCompliment2 = action.keyCompliment2;
-			newState.thirdColor = action.thirdColor;
-			newState.text1Color = action.text1Color;
-			newState.text2Color = action.text2Color;
-			newState.text3Color = action.text3Color;
-			newState.text4Color = action.text4Color;
-			newState.text5Color = action.text5Color;
-			newState.text6Color = action.text6Color;
-			newState.text7Color = action.text7Color;
-			newState.text8Color = action.text8Color;
-			newState.keyText1Color = action.keyText1Color;
-			newState.keyText2Color = action.keyText2Color;
-			newState.keyText3Color = action.keyText3Color;
-			newState.keyText4Color = action.keyText4Color;
-			newState.keyText5Color = action.keyText5Color;
-			newState.keyText6Color = action.keyText6Color;
-			newState.keyText7Color = action.keyText7Color;
-			newState.keyText8Color = action.keyText8Color;
-			newState = _getStateForSection(newState);
+			newState = _updateColorTheme(newState, action);
 			return newState;
 		case MOVE_TO_PROFILE:
 			newState.activeSection = 'profile';
