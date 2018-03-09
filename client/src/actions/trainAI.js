d8e237778c50542701f50a778cc85988e4958a96
Hunter Liu <Hunterzhaoliu@gmail.com>
2018-03-08 20:37:55 -0600

    y

--- client/src/actions/trainAI.js
+++ client/src/actions/trainAI.js
@@ -1,13 +1,6 @@
 import axios from 'axios';
 import { ON_VOTE } from './types';
 
-<<<<<<< HEAD
-export function onVote(answerChosen) {
-	return function(dispatch) {
-		dispatch({ type: ON_VOTE, answerChosen: answerChosen });
-	};
-}
-=======
 export const onVote = answerChoosen => dispatch => {
 	dispatch({ type: ON_VOTE, answerChoosen: answerChoosen });
 };
@@ -20,4 +13,3 @@ export const fetchUserTrainAIAsks = () => async dispatch => {
 	//     profile: response.data.profile
 	// });
 };
->>>>>>> 0fc78c83486c2a37420c1b3ce0ab307dc1d2308b
