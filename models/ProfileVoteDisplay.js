0ca7c12dc4d1ed73bfb718f49a7ffa10d3cd81d2
Q64 <quinnliu@vt.edu>
2018-03-11 17:31:44 -0500

    y

--- models/ProfileVoteDisplay.js
+++ models/ProfileVoteDisplay.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose; // = const Schema = mongoose.Schema;
 const profileVoteDisplaySchema = new Schema({
 	question: String,
 	_askId: { type: Schema.Types.ObjectId, ref: 'Ask' },
-	selectedAnswer: String
+	selectedAnswer: String,
+	_answerId: { type: Schema.Types.ObjectId }
 });
 
 module.exports = profileVoteDisplaySchema;
--- routes/trainAIRoutes.js
+++ routes/trainAIRoutes.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 
 const AskCollection = mongoose.model('asks');
+const UserCollection = mongoose.model('users');
 
 module.exports = app => {
 	app.get('/api/train_ai', requireLogin, async (request, response) => {
@@ -16,45 +17,81 @@ module.exports = app => {
 
 	app.put('/api/train_ai/vote', requireLogin, async (request, response) => {
 		const { answerId, askId } = request.body;
-		const askInDB = await AskCollection.findOne({ _id: askId });
 
-		//check if answerId = answerId in askInDB.answers
-		let answer;
-		for (let i = 0; i < askInDB.answers.length; i++) {
-			//need to convert to string in order to compare
-			if (String(askInDB.answers[i]._id) === String(answerId)) {
-				answer = askInDB.answers[i].answer;
-				askInDB.lastVotedOn = Date.now();
-				askInDB.answers[i].votes += 1;
-				askInDB.totalVotes += 1;
+		// detect if this vote is a revote
+		let isRevote = false;
+		let previousAnswerId;
+		const userInDB = await UserCollection.findOne({
+			_id: request.user._id
+		});
+
+		const askInDB = await AskCollection.findOne({ _id: askId });
+		const userVotedAsks = userInDB.profile.asks.votes;
+		for (let i = 0; i < userVotedAsks.length; i++) {
+			if (String(userVotedAsks[i]._askId) === String(askId)) {
+				previousAnswerId = userVotedAsks[i]._answerId;
+				isRevote = true;
 			}
 		}
 
-		try {
-			await AskCollection.updateOne(
-				{ _id: askId },
-				{
-					$set: {
-						lastVotedOn: askInDB.lastVotedOn,
-						answers: askInDB.answers,
-						totalVotes: askInDB.totalVotes
-					}
+		console.log('isRevote = ', isRevote);
+		if (isRevote) {
+			let answer;
+			for (let i = 0; i < askInDB.answers.length; i++) {
+				//need to convert to string in order to compare
+				if (String(askInDB.answers[i]._id) === String(answerId)) {
+					answer = askInDB.answers[i].answer;
+					askInDB.lastVotedOn = Date.now();
+					askInDB.answers[i].votes += 1;
 				}
-			);
+				if (
+					String(askInDB.answers[i]._id) === String(previousAnswerId)
+				) {
+					askInDB.answers[i].votes -= 1;
+				}
+			}
+		} else {
+			// updates the askInDB correctly
+			let answer;
+			let answerId;
+			for (let i = 0; i < askInDB.answers.length; i++) {
+				//need to convert to string in order to compare
+				if (String(askInDB.answers[i]._id) === String(answerId)) {
+					answer = askInDB.answers[i].answer;
+					answerId = askInDB.answers[i]._id;
+					askInDB.lastVotedOn = Date.now();
+					askInDB.answers[i].votes += 1;
+					askInDB.totalVotes += 1;
+				}
+			}
 
-			request.user.profile.asks.votes.push({
-				question: askInDB.question,
-				_askId: askInDB._id,
-				selectedAnswer: answer
-			});
-			const user = await request.user.save();
-			const responseObject = {
-				user,
-				askInDB
-			};
-			response.send(responseObject);
-		} catch (error) {
-			response.status(422).send(error);
+			try {
+				await AskCollection.updateOne(
+					{ _id: askId },
+					{
+						$set: {
+							lastVotedOn: askInDB.lastVotedOn,
+							answers: askInDB.answers,
+							totalVotes: askInDB.totalVotes
+						}
+					}
+				);
+
+				request.user.profile.asks.votes.push({
+					question: askInDB.question,
+					_askId: askInDB._id,
+					selectedAnswer: answer,
+					_answerId: answerId
+				});
+				const user = await request.user.save();
+				const responseObject = {
+					user,
+					askInDB
+				};
+				response.send(responseObject);
+			} catch (error) {
+				response.status(422).send(error);
+			}
 		}
 	});
 };
