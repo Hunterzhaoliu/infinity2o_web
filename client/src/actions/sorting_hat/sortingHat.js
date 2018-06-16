03eb9ba95894e2676d348727752fe10d909da78a
Q64 <quinnliu@vt.edu>
2018-06-16 18:30:56 -0500

    sortingHat fetchUserMatch fixed

--- client/src/actions/sorting_hat/sortingHat.js
+++ client/src/actions/sorting_hat/sortingHat.js
@@ -1,156 +1,157 @@
-import axios from "axios";
+import axios from 'axios';
 import {
-  ON_VOTE,
-  UPDATE_VOTED_ASK,
-  SAVE_FETCHED_INITIAL_ASKS,
-  SAVE_FETCHED_NEXT_ASKS,
-  UPDATE_INITIAL_4_ASKS,
-  SAVE_VOTE_START,
-  SAVE_VOTE_DONE,
-  SAVE_VOTE_ERROR,
-  ON_NEXT_ASK,
-  ON_NEWEST_ASKS,
-  ON_POPULAR_ASKS,
-  ON_CONTROVERSIAL_ASKS,
-  UPDATE_TOTAL_USER_VOTES_ACROSS_ALL_SESSIONS,
-  RUNNING_ATHENA_FOR_USER_START,
-  RUNNING_ATHENA_FOR_USER_DONE,
-  RUNNING_ATHENA_FOR_USER_ERROR
-} from "../types";
-import { MINIMUM_VOTES_TO_GET_IMMEDIATE_MATCH } from "../../utils/constants";
-import { store } from "../../index";
-import { fetchUserMatches } from "../matches/matches";
-import { saveAndAddNeurons } from "./ask";
-import { NUMBER_NEURONS_GIVEN_FOR_VOTE_IN_BILLIONS } from "../../containers/payment/prices";
+	ON_VOTE,
+	UPDATE_VOTED_ASK,
+	SAVE_FETCHED_INITIAL_ASKS,
+	SAVE_FETCHED_NEXT_ASKS,
+	UPDATE_INITIAL_4_ASKS,
+	SAVE_VOTE_START,
+	SAVE_VOTE_DONE,
+	SAVE_VOTE_ERROR,
+	ON_NEXT_ASK,
+	ON_NEWEST_ASKS,
+	ON_POPULAR_ASKS,
+	ON_CONTROVERSIAL_ASKS,
+	UPDATE_TOTAL_USER_VOTES_ACROSS_ALL_SESSIONS,
+	RUNNING_ATHENA_FOR_USER_START,
+	RUNNING_ATHENA_FOR_USER_DONE,
+	RUNNING_ATHENA_FOR_USER_ERROR
+} from '../types';
+import { MINIMUM_VOTES_TO_GET_IMMEDIATE_MATCH } from '../../utils/constants';
+import { store } from '../../index';
+import { fetchUserMatchesDispatch } from '../matches/matches';
+import { saveAndAddNeurons } from './ask';
+import { NUMBER_NEURONS_GIVEN_FOR_VOTE_IN_BILLIONS } from '../../containers/payment/prices';
 
 export const onNewestAsks = colorTheme => dispatch => {
-  dispatch({
-    type: ON_NEWEST_ASKS
-  });
+	dispatch({
+		type: ON_NEWEST_ASKS
+	});
 };
 
 export const onPopularAsks = colorTheme => dispatch => {
-  dispatch({
-    type: ON_POPULAR_ASKS
-  });
+	dispatch({
+		type: ON_POPULAR_ASKS
+	});
 };
 
 export const onControversialAsks = colorTheme => dispatch => {
-  dispatch({
-    type: ON_CONTROVERSIAL_ASKS
-  });
+	dispatch({
+		type: ON_CONTROVERSIAL_ASKS
+	});
 };
 
 export const onVote = (
-  answerIndex,
-  answerId,
-  askIndex,
-  askId,
-  history,
-  mongoDBUserId
+	answerIndex,
+	answerId,
+	askIndex,
+	askId,
+	history,
+	mongoDBUserId
 ) => async dispatch => {
-  dispatch({ type: SAVE_VOTE_START, saveIndex: askIndex });
-  dispatch({
-    type: ON_VOTE,
-    answerIndex: answerIndex,
-    askIndex: askIndex
-  });
+	dispatch({ type: SAVE_VOTE_START, saveIndex: askIndex });
+	dispatch({
+		type: ON_VOTE,
+		answerIndex: answerIndex,
+		askIndex: askIndex
+	});
 
-  const voteInfo = {
-    answerId: answerId,
-    askId: askId
-  };
-  const response = await axios.put("/api/sorting_hat/vote", voteInfo);
-  //response.data === askInDB
-  dispatch({
-    type: UPDATE_VOTED_ASK,
-    askIndex: askIndex,
-    newAsk: response.data
-  });
+	const voteInfo = {
+		answerId: answerId,
+		askId: askId
+	};
+	const response = await axios.put('/api/sorting_hat/vote', voteInfo);
+	//response.data === askInDB
+	dispatch({
+		type: UPDATE_VOTED_ASK,
+		askIndex: askIndex,
+		newAsk: response.data
+	});
 
-  if (response.status === 200) {
-    dispatch({ type: SAVE_VOTE_DONE, saveIndex: askIndex });
-    saveAndAddNeurons(
-      mongoDBUserId,
-      dispatch,
-      NUMBER_NEURONS_GIVEN_FOR_VOTE_IN_BILLIONS
-    );
-  } else {
-    dispatch({ type: SAVE_VOTE_ERROR, saveIndex: askIndex });
-  }
+	if (response.status === 200) {
+		dispatch({ type: SAVE_VOTE_DONE, saveIndex: askIndex });
+		saveAndAddNeurons(
+			mongoDBUserId,
+			dispatch,
+			NUMBER_NEURONS_GIVEN_FOR_VOTE_IN_BILLIONS
+		);
+	} else {
+		dispatch({ type: SAVE_VOTE_ERROR, saveIndex: askIndex });
+	}
 
-  // for running minerva the first time user logins and votes
-  dispatch({
-    type: UPDATE_TOTAL_USER_VOTES_ACROSS_ALL_SESSIONS,
-    additionalVotes: 1
-  });
+	// for running minerva the first time user logins and votes
+	dispatch({
+		type: UPDATE_TOTAL_USER_VOTES_ACROSS_ALL_SESSIONS,
+		additionalVotes: 1
+	});
 
-  if (
-    !store.getState().profile.ranInitialMinerva &&
-    store.getState().matches.totalUserVotesAcrossAllSessions >=
-      MINIMUM_VOTES_TO_GET_IMMEDIATE_MATCH
-  ) {
-    dispatch({
-      type: RUNNING_ATHENA_FOR_USER_START
-    });
-    history.push("/matches");
-    const info = {
-      mongoDBUserId: store.getState().auth.mongoDBUserId
-    };
+	if (
+		!store.getState().profile.ranInitialMinerva &&
+		store.getState().matches.totalUserVotesAcrossAllSessions >=
+			MINIMUM_VOTES_TO_GET_IMMEDIATE_MATCH
+	) {
+		dispatch({
+			type: RUNNING_ATHENA_FOR_USER_START
+		});
+		history.push('/matches');
+		const info = {
+			mongoDBUserId: store.getState().auth.mongoDBUserId
+		};
 
-    // runs athena
-    const initialMatchesResponse = await axios.post(
-      "/api/matches/initial",
-      info
-    );
+		// runs athena
+		const initialMatchesResponse = await axios.post(
+			'/api/matches/initial',
+			info
+		);
 
-    if (initialMatchesResponse.status === 200) {
-      fetchUserMatches(mongoDBUserId);
-      dispatch({
-        type: RUNNING_ATHENA_FOR_USER_DONE
-      });
-    } else {
-      dispatch({
-        type: RUNNING_ATHENA_FOR_USER_ERROR
-      });
-    }
-  }
+		if (initialMatchesResponse.status === 200) {
+			console.log('initialMatchesResponse.status === 200');
+			fetchUserMatchesDispatch(mongoDBUserId, dispatch);
+			dispatch({
+				type: RUNNING_ATHENA_FOR_USER_DONE
+			});
+		} else {
+			dispatch({
+				type: RUNNING_ATHENA_FOR_USER_ERROR
+			});
+		}
+	}
 };
 
 export const fetchUserSortingHatAsks = mongoDBUserId => async dispatch => {
-  const nextAsks = await axios.get(
-    "/api/sorting_hat/initial_asks?mongoDBUserId=" + mongoDBUserId
-  );
-  dispatch({
-    type: SAVE_FETCHED_INITIAL_ASKS,
-    nextAsks: nextAsks
-  });
-  dispatch({
-    type: UPDATE_INITIAL_4_ASKS
-  });
+	const nextAsks = await axios.get(
+		'/api/sorting_hat/initial_asks?mongoDBUserId=' + mongoDBUserId
+	);
+	dispatch({
+		type: SAVE_FETCHED_INITIAL_ASKS,
+		nextAsks: nextAsks
+	});
+	dispatch({
+		type: UPDATE_INITIAL_4_ASKS
+	});
 };
 
 export const onNextAsk = (
-  nextAsks,
-  removeAskIndex,
-  mongoDBUserId
+	nextAsks,
+	removeAskIndex,
+	mongoDBUserId
 ) => async dispatch => {
-  if (nextAsks.length < 1) {
-    const newNextAsks = await axios.get(
-      "/api/sorting_hat/next_asks?mongoDBUserId=" + mongoDBUserId
-    );
-    dispatch({
-      type: SAVE_FETCHED_NEXT_ASKS,
-      nextAsks: newNextAsks
-    });
-    dispatch({
-      type: ON_NEXT_ASK,
-      removeAskIndex: removeAskIndex
-    });
-  } else {
-    dispatch({
-      type: ON_NEXT_ASK,
-      removeAskIndex: removeAskIndex
-    });
-  }
+	if (nextAsks.length < 1) {
+		const newNextAsks = await axios.get(
+			'/api/sorting_hat/next_asks?mongoDBUserId=' + mongoDBUserId
+		);
+		dispatch({
+			type: SAVE_FETCHED_NEXT_ASKS,
+			nextAsks: newNextAsks
+		});
+		dispatch({
+			type: ON_NEXT_ASK,
+			removeAskIndex: removeAskIndex
+		});
+	} else {
+		dispatch({
+			type: ON_NEXT_ASK,
+			removeAskIndex: removeAskIndex
+		});
+	}
 };
--- routes/matchesRoutes.js
+++ routes/matchesRoutes.js
@@ -1,212 +1,230 @@
-const requireLogin = require("../middlewares/requireLogin");
-const mongoose = require("mongoose");
-const ConversationCollection = mongoose.model("conversations");
-const UserCollection = mongoose.model("users");
-const amqp = require("amqplib/callback_api");
+const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require('mongoose');
+const ConversationCollection = mongoose.model('conversations');
+const UserCollection = mongoose.model('users');
+const amqp = require('amqplib/callback_api');
 
 const getMatchesInfo = async mongoDBUserIds => {
-  let matchesInfo = [];
-  for (let mongoDBUserId of mongoDBUserIds) {
-    const user = await UserCollection.findById(mongoDBUserId);
+	let matchesInfo = [];
+	for (let mongoDBUserId of mongoDBUserIds) {
+		const user = await UserCollection.findById(mongoDBUserId);
 
-    if (user !== null) {
-      matchesInfo.push({
-        name: user.profile.name,
-        age: user.profile.age,
-        linkedInPublicProfileUrl: user.profile.linkedInPublicProfileUrl,
-        githubPublicProfileUrl: user.profile.githubPublicProfileUrl,
-        interests: user.profile.interests,
-        timeZone: user.profile.timeZone,
-        totalUserVotes: user.profile.asks.totalUserVotes,
-        availability: user.profile.availability,
-        asks: user.profile.asks,
-        id: user._id,
-        imageUrl: user.profile.imageUrl
-      });
-    }
-  }
-  return matchesInfo;
+		if (user !== null) {
+			matchesInfo.push({
+				name: user.profile.name,
+				age: user.profile.age,
+				linkedInPublicProfileUrl: user.profile.linkedInPublicProfileUrl,
+				githubPublicProfileUrl: user.profile.githubPublicProfileUrl,
+				interests: user.profile.interests,
+				timeZone: user.profile.timeZone,
+				totalUserVotes: user.profile.asks.totalUserVotes,
+				availability: user.profile.availability,
+				asks: user.profile.asks,
+				id: user._id,
+				imageUrl: user.profile.imageUrl
+			});
+		}
+	}
+	return matchesInfo;
 };
 
 const generateUniqueUID = () => {
-  return (
-    Math.random().toString() +
-    Math.random().toString() +
-    Math.random().toString()
-  );
+	return (
+		Math.random().toString() +
+		Math.random().toString() +
+		Math.random().toString()
+	);
 };
 
 module.exports = app => {
-  app.get("/api/matches", requireLogin, async (request, response) => {
-    // formats the request string into an array
-    const mongoDBUserId = request.query.mongoDBUserId;
+	app.get('/api/matches', requireLogin, async (request, response) => {
+		// formats the request string into an array
+		const mongoDBUserId = request.query.mongoDBUserId;
+		// get user's Match Ids
+		const userInDB = await UserCollection.findOne({ _id: mongoDBUserId });
+		const userMatches = userInDB.matches;
+		let mongoDBMatchIds = [];
+		for (let i = 0; i < userMatches.length; i++) {
+			mongoDBMatchIds.push(userMatches[i]['id']);
+		}
 
-    // get user's Match Ids
-    const userInDB = await UserCollection.findOne({ _id: mongoDBUserId });
-    const userMatches = userInDB.matches;
-    let mongoDBMatchIds = [];
-    for (let i = 0; i < userMatches.length; i++) {
-      mongoDBMatchIds.push(userMatches[i]["id"]);
-    }
+		let matchesInfo = await getMatchesInfo(mongoDBMatchIds);
+		response.send(matchesInfo);
+	});
 
-    let matchesInfo = await getMatchesInfo(mongoDBMatchIds);
-    response.send(matchesInfo);
-  });
+	app.post(
+		'/api/matches/start_conversation',
+		requireLogin,
+		async (request, response) => {
+			const { matchId, matchName } = request.body;
 
-  app.post(
-    "/api/matches/start_conversation",
-    requireLogin,
-    async (request, response) => {
-      const { matchId, matchName } = request.body;
+			const userId = request.user._id;
+			let userName;
+			if (request.user.profile.name === undefined) {
+				userName = 'Anonymous';
+			} else {
+				userName = request.user.profile.name;
+			}
 
-      const userId = request.user._id;
-      let userName;
-      if (request.user.profile.name === undefined) {
-        userName = "Anonymous";
-      } else {
-        userName = request.user.profile.name;
-      }
+			// sets new conversation document to conversation variable
+			const conversation = new ConversationCollection({
+				user1: { name: userName, id: userId },
+				user2: { name: matchName, id: matchId },
+				last50Messages: []
+			});
 
-      // sets new conversation document to conversation variable
-      const conversation = new ConversationCollection({
-        user1: { name: userName, id: userId },
-        user2: { name: matchName, id: matchId },
-        last50Messages: []
-      });
+			// finds userConversationList && matchConversationList
+			const userInDB = await UserCollection.findOne({ _id: userId });
+			let userConversationList = userInDB.conversations;
+			// console.log('userConversationList = ', userConversationList);
+			const matchInDB = await UserCollection.findOne({ _id: matchId });
+			let matchConversationList = matchInDB.conversations;
+			// console.log('matchConversationList = ', matchConversationList);
 
-      // finds userConversationList && matchConversationList
-      const userInDB = await UserCollection.findOne({ _id: userId });
-      let userConversationList = userInDB.conversations;
-      // console.log('userConversationList = ', userConversationList);
-      const matchInDB = await UserCollection.findOne({ _id: matchId });
-      let matchConversationList = matchInDB.conversations;
-      // console.log('matchConversationList = ', matchConversationList);
+			try {
+				const conversationInDB = await conversation.save();
+				// console.log('conversationInDB = ', conversationInDB);
+				userConversationList.push({
+					conversationId: conversationInDB._id,
+					matchName: matchName,
+					matchId: matchId,
+					isOnline: false,
+					socketId: null
+				});
+				await UserCollection.updateOne(
+					{ _id: userId },
+					{ $set: { conversations: userConversationList } },
+					{ upsert: true }
+				);
+				matchConversationList.push({
+					conversationId: conversationInDB._id,
+					matchName: userName,
+					matchId: userId,
+					isOnline: false,
+					socketId: null
+				});
 
-      try {
-        const conversationInDB = await conversation.save();
-        // console.log('conversationInDB = ', conversationInDB);
-        userConversationList.push({
-          conversationId: conversationInDB._id,
-          matchName: matchName,
-          matchId: matchId,
-          isOnline: false,
-          socketId: null
-        });
-        await UserCollection.updateOne(
-          { _id: userId },
-          { $set: { conversations: userConversationList } },
-          { upsert: true }
-        );
-        matchConversationList.push({
-          conversationId: conversationInDB._id,
-          matchName: userName,
-          matchId: userId,
-          isOnline: false,
-          socketId: null
-        });
+				await UserCollection.updateOne(
+					{ _id: matchId },
+					{ $set: { conversations: matchConversationList } },
+					{ upsert: true }
+				);
+				response.send(userConversationList);
+			} catch (error) {
+				response.status(422).send(error);
+			}
+		}
+	);
 
-        await UserCollection.updateOne(
-          { _id: matchId },
-          { $set: { conversations: matchConversationList } },
-          { upsert: true }
-        );
-        response.send(userConversationList);
-      } catch (error) {
-        response.status(422).send(error);
-      }
-    }
-  );
+	app.delete(
+		'/api/matches/delete_match',
+		requireLogin,
+		async (request, response) => {
+			const { matchId } = request.body;
+			const userId = request.user._id;
 
-  app.delete(
-    "/api/matches/delete_match",
-    requireLogin,
-    async (request, response) => {
-      const { matchId } = request.body;
-      const userId = request.user._id;
+			let userMatchesDict = await UserCollection.findOne(
+				{ _id: userId },
+				{ matches: true, _id: false }
+			);
 
-      let userMatchesDict = await UserCollection.findOne(
-        { _id: userId },
-        { matches: true, _id: false }
-      );
+			let userMatches = userMatchesDict.matches;
+			let matchIndex = userMatches.indexOf(matchId);
 
-      let userMatches = userMatchesDict.matches;
-      let matchIndex = userMatches.indexOf(matchId);
+			userMatches.splice(matchIndex, 1);
 
-      userMatches.splice(matchIndex, 1);
+			// let userMatches = userInDB.matches;
+			const matchInDB = await UserCollection.findOne({ _id: matchId });
+			let matchMatches = matchInDB.matches;
 
-      // let userMatches = userInDB.matches;
-      const matchInDB = await UserCollection.findOne({ _id: matchId });
-      let matchMatches = matchInDB.matches;
+			let userIndex = matchMatches.indexOf(userId);
+			if (userIndex > -1) {
+				matchMatches.splice(userIndex, 1);
+			}
 
-      let userIndex = matchMatches.indexOf(userId);
-      if (userIndex > -1) {
-        matchMatches.splice(userIndex, 1);
-      }
+			try {
+				await UserCollection.updateOne(
+					{ _id: userId },
+					{ $set: { matches: userMatches } }
+				);
+				await UserCollection.updateOne(
+					{ _id: matchId },
+					{ $set: { matches: matchMatches } }
+				);
+				response.send('successful');
+			} catch (error) {
+				response.status(422).send(error);
+			}
+		}
+	);
 
-      try {
-        await UserCollection.updateOne(
-          { _id: userId },
-          { $set: { matches: userMatches } }
-        );
-        await UserCollection.updateOne(
-          { _id: matchId },
-          { $set: { matches: matchMatches } }
-        );
-        response.send("successful");
-      } catch (error) {
-        response.status(422).send(error);
-      }
-    }
-  );
+	app.post(
+		'/api/matches/initial',
+		requireLogin,
+		async (request, response) => {
+			// send message to minerva server to run minerva
+			// for a specific user
+			const { mongoDBUserId } = request.body;
+			const URL = 'amqp://infinity2o:2134711@52.4.101.52:5672';
+			await amqp.connect(URL, function(error, connection) {
+				connection.createChannel(function(error, channel) {
+					let sendToQueueName = 'run_athena_for_new_user_queue';
+					channel.assertQueue('', { exclusive: true }, function(
+						error,
+						replyToQueueObject
+					) {
+						const correlationId = generateUniqueUID();
 
-  app.post("/api/matches/initial", requireLogin, async (request, response) => {
-    // send message to minerva server to run minerva
-    // for a specific user
-    const { mongoDBUserId } = request.body;
-    const URL = "amqp://infinity2o:2134711@52.4.101.52:5672";
-    await amqp.connect(URL, function(error, connection) {
-      connection.createChannel(function(error, channel) {
-        let sendToQueueName = "run_athena_for_new_user_queue";
-        channel.assertQueue("", { exclusive: true }, function(
-          error,
-          replyToQueueObject
-        ) {
-          const correlationId = generateUniqueUID();
+						console.log(
+							' [x] Requesting Athena matches for mongoDBUserId=%s',
+							mongoDBUserId
+						);
 
-          console.log(
-            " [x] Requesting Athena matches for mongoDBUserId=%s",
-            mongoDBUserId
-          );
+						channel.consume(
+							replyToQueueObject.queue,
+							function(messageFromServer) {
+								if (
+									messageFromServer.properties
+										.correlationId == correlationId
+								) {
+									const mongoDBUserIdFromAthenaServer = messageFromServer.content.toString();
+									console.log(
+										' [.] Got messageFromServer=%s',
+										mongoDBUserIdFromAthenaServer
+									);
 
-          channel.consume(
-            replyToQueueObject.queue,
-            function(messageFromServer) {
-              if (messageFromServer.properties.correlationId == correlationId) {
-                const mongoDBUserIdFromAthenaServer = messageFromServer.content.toString();
-                console.log(
-                  " [.] Got messageFromServer=%s",
-                  mongoDBUserIdFromAthenaServer
-                );
+									if (
+										mongoDBUserIdFromAthenaServer !== null
+									) {
+										response.send(
+											mongoDBUserIdFromAthenaServer
+										);
+									} else {
+										response
+											.status(422)
+											.send(
+												'ERROR: Athena ran unsuccessfully'
+											);
+									}
+									setTimeout(function() {
+										connection.close();
+									}, 500);
+								}
+							},
+							{ noAck: true }
+						);
 
-                if (mongoDBUserIdFromAthenaServer !== null) {
-                  response.send(mongoDBUserIdFromAthenaServer);
-                } else {
-                  response.status(422).send("ERROR: Athena ran unsuccessfully");
-                }
-                setTimeout(function() {
-                  connection.close();
-                }, 500);
-              }
-            },
-            { noAck: true }
-          );
-
-          channel.sendToQueue(sendToQueueName, new Buffer(mongoDBUserId), {
-            correlationId: correlationId,
-            replyTo: replyToQueueObject.queue
-          });
-        });
-      });
-    });
-  });
+						channel.sendToQueue(
+							sendToQueueName,
+							new Buffer(mongoDBUserId),
+							{
+								correlationId: correlationId,
+								replyTo: replyToQueueObject.queue
+							}
+						);
+					});
+				});
+			});
+		}
+	);
 };
