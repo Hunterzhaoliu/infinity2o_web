23faa2cc8dd400d6cb7c5b6f286e09151e1725c9
Q64 <quinnliu@vt.edu>
2018-06-16 19:13:38 -0500

    y

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './containers/App';
 import reducers from './reducers';
+
+const persistConfig = {
+	key: 'root',
+	storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 // development only
 // import axios from 'axios';
@@ -13,14 +23,17 @@ import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
-	reducers,
+	persistedReducer,
 	{},
 	composeEnhancers(applyMiddleware(reduxThunk))
 );
+let persistor = persistStore(store);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>,
 	document.querySelector('#root')
 );
