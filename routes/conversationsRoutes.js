394439ae80adb9ba1cea8ba6701cc1d37e801138
Hunter Liu <Hunterzhaoliu@gmail.com>
2018-07-24 11:28:32 -0500

    moved the update user conversations with online contacts to conversationsRoutes

--- routes/conversationsRoutes.js
+++ routes/conversationsRoutes.js
@@ -1,133 +1,162 @@
-const requireLogin = require('../middlewares/requireLogin');
-const mongoose = require('mongoose');
-const ConversationCollection = mongoose.model('conversations');
+const _ = require("lodash");
+const requireLogin = require("../middlewares/requireLogin");
+const mongoose = require("mongoose");
+const ConversationCollection = mongoose.model("conversations");
 
 const tellContactsUserIsOnline = async (
-	userConversations,
-	mongoDBUserId,
-	serverSocket,
-	clientSocketId,
-	redis
+  userConversations,
+  mongoDBUserId,
+  serverSocket,
+  clientSocketId,
+  redis
 ) => {
-	// tell all the user's contacts that are already online that the user is online
-	for (let i = 0; i < userConversations.length; i++) {
-		const contactMongoDBUserId = userConversations[i]['matchId'];
-		redis.get(contactMongoDBUserId, function(err, reply) {
-			if (reply !== null) {
-				const contactSocketId = reply.toString();
-				// contact is online
-				const newUserSocketInfo = {
-					userId: mongoDBUserId,
-					socketId: clientSocketId
-				};
+  // tell all the user's contacts that are already online that the user is online
+  for (let i = 0; i < userConversations.length; i++) {
+    const contactMongoDBUserId = userConversations[i]["matchId"];
+    redis.get(contactMongoDBUserId, function(err, reply) {
+      if (reply !== null) {
+        const contactSocketId = reply.toString();
+        // contact is online
+        const newUserSocketInfo = {
+          userId: mongoDBUserId,
+          socketId: clientSocketId
+        };
 
-				// send message to contact telling them we are also online
-				serverSocket
-					.to(contactSocketId)
-					.emit(
-						'TELL_CONTACT_X:ONE_OF_YOUR_CONTACTS_IS_ONLINE',
-						newUserSocketInfo
-					);
-				// console.log(
-				// 	'updated contactMongoDBUserId = ' +
-				// 		contactMongoDBUserId +
-				// 		' with our socketId'
-				// );
-			}
-		});
-	}
+        // send message to contact telling them we are also online
+        serverSocket
+          .to(contactSocketId)
+          .emit(
+            "TELL_CONTACT_X:ONE_OF_YOUR_CONTACTS_IS_ONLINE",
+            newUserSocketInfo
+          );
+        // console.log(
+        // 	'updated contactMongoDBUserId = ' +
+        // 		contactMongoDBUserId +
+        // 		' with our socketId'
+        // );
+      }
+    });
+  }
+};
+
+const updateUserConversationsWithOnlineContacts = async (
+  userConversations,
+  redis
+) => {
+  _.map(userConversations, userConversation => {
+    // need to redo this redis.get because await is not working
+    redis.get(userConversation.matchId, function(err, reply) {
+      if (reply !== null) {
+        const contactSocketId = reply.toString();
+        // the current contact is online
+        userConversation["isOnline"] = true;
+        userConversation["socketId"] = contactSocketId;
+      } else if (reply === null) {
+        // TODO: remove when we actually figure out how to detect user is offline
+        userConversation["isOnline"] = false;
+        userConversation["socketId"] = null;
+      }
+    });
+  });
+
+  return userConversations;
 };
 
 module.exports = app => {
-	app.get('/api/conversations', requireLogin, async (request, response) => {
-		const conversation = await ConversationCollection.findOne({
-			_id: request.query.conversationId
-		});
-		response.send(conversation);
-	});
+  app.get("/api/conversations", requireLogin, async (request, response) => {
+    const conversation = await ConversationCollection.findOne({
+      _id: request.query.conversationId
+    });
+    response.send(conversation);
+  });
 
-	app.post(
-		'/api/conversations/user_online',
-		requireLogin,
-		async (request, response) => {
-			const {
-				mongoDBUserId,
-				userConversations,
-				clientSocketId
-			} = request.body;
+  app.post(
+    "/api/conversations/user_online",
+    requireLogin,
+    async (request, response) => {
+      const { mongoDBUserId, userConversations, clientSocketId } = request.body;
 
-			const redis = request.app.get('redis');
+      const redis = request.app.get("redis");
 
-			redis.get(mongoDBUserId, function(err, reply) {
-				if (reply !== null) {
-					const oldClientSocketId = reply.toString();
-					redis.del(oldClientSocketId);
-				}
-			});
+      redis.get(mongoDBUserId, function(err, reply) {
+        if (reply !== null) {
+          const oldClientSocketId = reply.toString();
+          redis.del(oldClientSocketId);
+        }
+      });
 
-			redis.set(mongoDBUserId, clientSocketId);
-			redis.set(clientSocketId, mongoDBUserId);
+      redis.set(mongoDBUserId, clientSocketId);
+      redis.set(clientSocketId, mongoDBUserId);
 
-			const serverSocket = request.app.get('serverSocket');
+      const serverSocket = request.app.get("serverSocket");
 
-			tellContactsUserIsOnline(
-				userConversations,
-				mongoDBUserId,
-				serverSocket,
-				clientSocketId,
-				redis
-			);
-			response.send("added user's serverSocketId to redis");
-		}
-	);
+      tellContactsUserIsOnline(
+        userConversations,
+        mongoDBUserId,
+        serverSocket,
+        clientSocketId,
+        redis
+      );
+      response.send("added user's serverSocketId to redis");
+    }
+  );
 
-	app.post(
-		'/api/conversations/chat',
-		requireLogin,
-		async (request, response) => {
-			const {
-				conversationId,
-				senderName,
-				message,
-				timeCreated
-			} = request.body;
+  app.put(
+    "/api/conversations/online_contacts",
+    requireLogin,
+    async (request, response) => {
+      const redis = request.app.get("redis");
+      const updatedUserConversations = await updateUserConversationsWithOnlineContacts(
+        request.body,
+        redis
+      );
 
-			// GOAL = save newMessage into correct conversation document
-			const newMessage = {
-				senderName: senderName,
-				content: message,
-				timeCreated: timeCreated,
-				status: 'delivered'
-			};
+      response.send(updatedUserConversations);
+    }
+  );
 
-			const conversation = await ConversationCollection.findOne(
-				{
-					_id: conversationId
-				},
-				{ last50Messages: true }
-			);
-			let last50Messages = conversation.last50Messages;
-			last50Messages.push(newMessage);
-			if (last50Messages.length > 50) {
-				last50Messages.shift();
-			}
+  app.post(
+    "/api/conversations/chat",
+    requireLogin,
+    async (request, response) => {
+      const { conversationId, senderName, message, timeCreated } = request.body;
 
-			try {
-				await ConversationCollection.updateOne(
-					{
-						_id: conversationId
-					},
-					{
-						$set: {
-							last50Messages: last50Messages
-						}
-					}
-				);
+      // GOAL = save newMessage into correct conversation document
+      const newMessage = {
+        senderName: senderName,
+        content: message,
+        timeCreated: timeCreated,
+        status: "delivered"
+      };
 
-				response.send(last50Messages);
-			} catch (error) {
-				response.status(422).send(error);
-			}
-		}
-	);
+      const conversation = await ConversationCollection.findOne(
+        {
+          _id: conversationId
+        },
+        { last50Messages: true }
+      );
+      let last50Messages = conversation.last50Messages;
+      last50Messages.push(newMessage);
+      if (last50Messages.length > 50) {
+        last50Messages.shift();
+      }
+
+      try {
+        await ConversationCollection.updateOne(
+          {
+            _id: conversationId
+          },
+          {
+            $set: {
+              last50Messages: last50Messages
+            }
+          }
+        );
+
+        response.send(last50Messages);
+      } catch (error) {
+        response.status(422).send(error);
+      }
+    }
+  );
 };
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,123 +1,74 @@
-const _ = require('lodash');
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const UserCollection = mongoose.model('users');
-
-const updateUserConversationsWithOnlineContacts = async (
-	userConversations,
-	redis
-) => {
-	// https://stackoverflow.com/questions/29693469/node-js-wait-for-all-redis-queries-to-complete-before-continuing-with-execution
-	await Promise.all(
-		_.map(userConversations, userConversation => {
-			return new Promise(function(resolve, reject) {
-				// need to redo this redis.get because await is not working
-				redis.get(userConversation.matchId, function(err, reply) {
-					if (err) {
-						return reject(err);
-					} else if (reply !== null) {
-						const contactSocketId = reply.toString();
-						// the current contact is online
-						userConversation['isOnline'] = true;
-						userConversation['socketId'] = contactSocketId;
-					}
-					resolve();
-				});
-			});
-		})
-	)
-		.then(function() {
-			// console.log('All operations are done');
-		})
-		.catch(function(err) {
-			console.log(err);
-		});
-
-	return userConversations;
-};
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const UserCollection = mongoose.model("users");
 
 module.exports = app => {
-	app.post('/api/profile', requireLogin, async (request, response) => {
-		const {
-			newName,
-			newEmail,
-			newAge,
-			newInterests,
-			newLinkedInPublicProfileUrl,
-			newGithubPublicProfileUrl,
-			newTimeZone,
-			newAvailability
-		} = request.body;
+  app.post("/api/profile", requireLogin, async (request, response) => {
+    const {
+      newName,
+      newEmail,
+      newAge,
+      newInterests,
+      newLinkedInPublicProfileUrl,
+      newGithubPublicProfileUrl,
+      newTimeZone,
+      newAvailability
+    } = request.body;
 
-		request.user.profile.name = newName;
-		request.user.profile.email = newEmail;
-		request.user.profile.age = newAge;
-		request.user.profile.interests = newInterests;
-		request.user.profile.linkedInPublicProfileUrl = newLinkedInPublicProfileUrl;
-		request.user.profile.githubPublicProfileUrl = newGithubPublicProfileUrl;
-		request.user.profile.timeZone = newTimeZone;
-		request.user.profile.availability = newAvailability;
+    request.user.profile.name = newName;
+    request.user.profile.email = newEmail;
+    request.user.profile.age = newAge;
+    request.user.profile.interests = newInterests;
+    request.user.profile.linkedInPublicProfileUrl = newLinkedInPublicProfileUrl;
+    request.user.profile.githubPublicProfileUrl = newGithubPublicProfileUrl;
+    request.user.profile.timeZone = newTimeZone;
+    request.user.profile.availability = newAvailability;
 
-		const user = await request.user.save();
-		response.send(user);
-	});
+    const user = await request.user.save();
+    response.send(user);
+  });
 
-	app.put(
-		'/api/profile/conversations',
-		requireLogin,
-		async (request, response) => {
-			const redis = request.app.get('redis');
-			const updatedUserConversations = await updateUserConversationsWithOnlineContacts(
-				request.body,
-				redis
-			);
-			request.user.conversations = updatedUserConversations;
-			await request.user.save();
-			response.send(updatedUserConversations);
-		}
-	);
+  app.put(
+    "/api/profile/decrease_neurons",
+    requireLogin,
+    async (request, response) => {
+      const { decrementAmount, mongoDBUserId } = request.body;
 
-	app.put(
-		'/api/profile/decrease_neurons',
-		requireLogin,
-		async (request, response) => {
-			const { decrementAmount, mongoDBUserId } = request.body;
+      try {
+        await UserCollection.findOneAndUpdate(
+          { _id: mongoDBUserId },
+          {
+            $inc: {
+              "profile.payment.neuronsInBillions": -decrementAmount
+            }
+          }
+        );
+        response.send("done");
+      } catch (error) {
+        response.status(422).send(error);
+      }
+    }
+  );
 
-			try {
-				await UserCollection.findOneAndUpdate(
-					{ _id: mongoDBUserId },
-					{
-						$inc: {
-							'profile.payment.neuronsInBillions': -decrementAmount
-						}
-					}
-				);
-				response.send('done');
-			} catch (error) {
-				response.status(422).send(error);
-			}
-		}
-	);
+  app.put(
+    "/api/profile/add_neurons",
+    requireLogin,
+    async (request, response) => {
+      const { neuronsInBillions, mongoDBUserId } = request.body;
 
-	app.put(
-		'/api/profile/add_neurons',
-		requireLogin,
-		async (request, response) => {
-			const { neuronsInBillions, mongoDBUserId } = request.body;
-
-			try {
-				await UserCollection.findOneAndUpdate(
-					{ _id: mongoDBUserId },
-					{
-						$inc: {
-							'profile.payment.neuronsInBillions': neuronsInBillions
-						}
-					}
-				);
-				response.send('done');
-			} catch (error) {
-				response.status(422).send(error);
-			}
-		}
-	);
+      try {
+        await UserCollection.findOneAndUpdate(
+          { _id: mongoDBUserId },
+          {
+            $inc: {
+              "profile.payment.neuronsInBillions": neuronsInBillions
+            }
+          }
+        );
+        response.send("done");
+      } catch (error) {
+        response.status(422).send(error);
+      }
+    }
+  );
 };
