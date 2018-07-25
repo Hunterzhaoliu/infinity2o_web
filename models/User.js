cacece03969ca4ff4d4d3e0d4613df840f6f0ee4
Hunter Liu <Hunterzhaoliu@gmail.com>
2018-07-24 22:09:45 -0500

    trying strategy 2 for conversations-badge

--- models/User.js
+++ models/User.js
@@ -6,63 +6,69 @@ const UserConversation = require("./UserConversation");
 const Match = require("./Match");
 
 const userSchema = new Schema(
-  {
-    auth: {
-      googleId: String,
-      linkedInId: String,
-      location: String
-    },
-    profile: {
-      colorTheme: {
-        savedBackgroundColor: {
-          type: String,
-          default: "rgb(229, 229, 229)"
-        },
-        savedColorPallateIndex: { type: Number, default: 4 }
-      },
-      name: String,
-      email: String,
-      imageUrl: String,
-      linkedInPublicProfileUrl: String,
-      githubPublicProfileUrl: String,
-      age: { type: Number },
-      interests: [String],
-      timeZone: [String],
-      availability: {
-        monday: [String],
-        tuesday: [String],
-        wednesday: [String],
-        thursday: [String],
-        friday: [String],
-        saturday: [String],
-        sunday: [String]
-      },
-      asks: {
-        questions: [ProfileQuestionDisplay],
-        votes: [ProfileVoteDisplay],
-        answerIdsUserVotedOn: [Schema.Types.ObjectId],
-        totalUserVotes: { type: Number, default: 0 }
-      },
-      payment: {
-        infinityStatus: Boolean,
-        neuronsInBillions: {
-          type: Number,
-          default: 0.22
-        }
-      },
-      minerva: {
-        ranInitialMinerva: { type: Boolean, default: false }
-        // after a new user votes on 8 asks we run
-        // minerva for them for the first time immediately
-      }
-    },
-    matches: [Match],
-    conversations: [UserConversation]
-  },
-  {
-    usePushEach: true
-    // allows for request.user.save()
-  }
+	{
+		auth: {
+			googleId: String,
+			linkedInId: String,
+			location: String
+		},
+		profile: {
+			colorTheme: {
+				savedBackgroundColor: {
+					type: String,
+					default: "rgb(229, 229, 229)"
+				},
+				savedColorPallateIndex: { type: Number, default: 4 }
+			},
+			name: String,
+			email: String,
+			imageUrl: String,
+			linkedInPublicProfileUrl: String,
+			githubPublicProfileUrl: String,
+			age: { type: Number },
+			interests: [String],
+			timeZone: [String],
+			availability: {
+				monday: [String],
+				tuesday: [String],
+				wednesday: [String],
+				thursday: [String],
+				friday: [String],
+				saturday: [String],
+				sunday: [String]
+			},
+			asks: {
+				questions: [ProfileQuestionDisplay],
+				votes: [ProfileVoteDisplay],
+				answerIdsUserVotedOn: [Schema.Types.ObjectId],
+				totalUserVotes: { type: Number, default: 0 }
+			},
+			payment: {
+				infinityStatus: Boolean,
+				neuronsInBillions: {
+					type: Number,
+					default: 0.22
+				}
+			},
+			minerva: {
+				ranInitialMinerva: { type: Boolean, default: false }
+				// after a new user votes on 8 asks we run
+				// minerva for them for the first time immediately
+			}
+		},
+		matches: [Match],
+		conversations: {
+			userConversations: [UserConversation],
+			totalNumberOfUnseenMessagesOnLastLogin: {
+				type: Number,
+				default: 0
+			}
+		}
+	},
+	{
+		usePushEach: true
+		// allows for request.user.save()
+	}
 );
 
 mongoose.model("users", userSchema);
--- models/UserConversation.js
+++ models/UserConversation.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose; // = const Schema = mongoose.Schema;
 
 const userConversationSchema = new Schema({
-	conversationId: { type: Schema.Types.ObjectId, ref: 'Conversation' },
+	conversationId: { type: Schema.Types.ObjectId, ref: "Conversation" },
 	matchName: String,
-	matchId: { type: Schema.Types.ObjectId, ref: 'User' },
+	matchId: { type: Schema.Types.ObjectId, ref: "User" },
 	isOnline: Boolean,
-	socketId: String
+	socketId: String,
+	numberOfUnseenMatches: { type: Number, default: 0 }
 });
 
 module.exports = userConversationSchema;
