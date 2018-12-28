94ca4c90dd21eeec04cb9fb87b4b133acfb8e92c
Q64 <hzl@seas.upenn.edu>
2018-12-27 18:15:08 -0600

    begin remodeling profile page

--- client/src/containers/profile/DisplayField.js
+++ client/src/containers/profile/DisplayField.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { GREY_2 } from '../styles/ColorConstants';
-import { Row, Col, Table, Popover, Button, Icon } from 'antd';
-import './DisplayField.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { GREY_2 } from "../styles/ColorConstants";
+import { Row, Col, Table, Popover, Button, Icon } from "antd";
+import "./DisplayField.css";
 
 class DisplayField extends Component {
 	numberWithCommas = x => {
-		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	};
 
 	renderNeuronExplanation() {
@@ -24,7 +24,7 @@ class DisplayField extends Component {
 			<div>
 				<p
 					style={{
-						padding: '7px 0px 0px',
+						padding: "7px 0px 0px",
 						color: colorTheme.text2Color
 					}}
 				>
@@ -47,14 +47,14 @@ class DisplayField extends Component {
 						borderColor: colorTheme.text8Color,
 						backgroundColor: colorTheme.text8Color,
 						color: colorTheme.text3Color,
-						padding: '0px 5px 0px'
+						padding: "0px 5px 0px"
 					}}
 					size="small"
 				>
 					<Icon
 						style={{
 							fontSize: 12,
-							padding: '0px 0px 6px'
+							padding: "0px 0px 6px"
 						}}
 						type="question-circle-o"
 					/>
@@ -87,13 +87,13 @@ class DisplayField extends Component {
 		);
 
 		if (
-			label === 'Neurons: ' ||
-			label === 'Name: ' ||
-			label === 'E-mail: ' ||
-			label === 'Age: ' ||
-			label === 'Time Zone: '
+			label === "Neurons: " ||
+			label === "Name: " ||
+			label === "E-mail: " ||
+			label === "Age: " ||
+			label === "Time Zone: "
 		) {
-			if (label === 'Neurons: ' && infinityStatus) {
+			if (label === "Neurons: " && infinityStatus) {
 				return (
 					<div>
 						<Col>
@@ -102,22 +102,22 @@ class DisplayField extends Component {
 						<Col>{this.renderNeuronExplanation()}</Col>
 					</div>
 				);
-			} else if (label === 'Neurons: ' && !infinityStatus) {
+			} else if (label === "Neurons: " && !infinityStatus) {
 				let displayNeuronsInBillions = value;
 				if (displayNeuronsInBillions !== undefined) {
 					displayNeuronsInBillions *= 1000000000;
 					let finalDisplayString =
 						this.numberWithCommas(displayNeuronsInBillions) +
-						' (' +
+						" (" +
 						value +
-						') Billion Neurons';
+						") Billion Neurons";
 
 					return (
 						<Row type="flex" justify="start" align="middle">
-							<Col style={{ padding: '25px 0px 0px' }}>
+							<Col style={{ padding: "25px 0px 0px" }}>
 								<p>{finalDisplayString}</p>
 							</Col>
-							<Col offset={1} style={{ padding: '7px 0px 0px' }}>
+							<Col offset={1} style={{ padding: "7px 0px 0px" }}>
 								{this.renderNeuronExplanation()}
 							</Col>
 						</Row>
@@ -126,156 +126,21 @@ class DisplayField extends Component {
 			} else if (
 				value === null ||
 				value === undefined ||
-				typeof value === 'string' ||
-				typeof value === 'number'
+				typeof value === "string" ||
+				typeof value === "number"
 			) {
 				return value;
 			}
-		} else if (label === 'Availability: ') {
-			// value = profile.availability
-			if (Object.keys(value).length !== 0) {
-				const daysOfWeek = [
-					'monday',
-					'tuesday',
-					'wednesday',
-					'thursday',
-					'friday',
-					'saturday',
-					'sunday'
-				];
-
-				// determines the width of the table & checks if user has timeSlots
-				let numberOfDaysAvailable = 0;
-
-				daysOfWeek.forEach(day => {
-					if (value[day].length > 0) {
-						numberOfDaysAvailable += 1;
-					}
-				});
-				const widthOfTable = numberOfDaysAvailable * 75 + 'px';
-				document.documentElement.style.setProperty(
-					`--widthOfTable`,
-					widthOfTable
-				);
-				if (numberOfDaysAvailable === 0) {
-					return;
-				}
-
-				// sets up availability table
-				let columnHeaders = [
-					{
-						title: 'Mon',
-						dataIndex: 'monday',
-						align: 'center'
-					},
-					{
-						title: 'Tues',
-						dataIndex: 'tuesday',
-						align: 'center'
-					},
-					{
-						title: 'Wed',
-						dataIndex: 'wednesday',
-						align: 'center'
-					},
-					{
-						title: 'Thurs',
-						dataIndex: 'thursday',
-						align: 'center'
-					},
-					{
-						title: 'Fri',
-						dataIndex: 'friday',
-						align: 'center'
-					},
-					{
-						title: 'Sat',
-						dataIndex: 'saturday',
-						align: 'center'
-					},
-					{
-						title: 'Sun',
-						dataIndex: 'sunday',
-						align: 'center'
-					}
-				];
-
-				const indexInTimeSlot = {
-					'6-8 AM': 0,
-					'8-10 AM': 1,
-					'10-12 AM': 2,
-					'12-2 PM': 3,
-					'2-4 PM': 4,
-					'4-6 PM': 5,
-					'6-8 PM': 6,
-					'8-10 PM': 7,
-					'10-12 PM': 8
-				};
-
-				let timeSlots = [
-					{ key: 0 },
-					{ key: 1 },
-					{ key: 2 },
-					{ key: 3 },
-					{ key: 4 },
-					{ key: 5 },
-					{ key: 6 },
-					{ key: 7 },
-					{ key: 8 }
-				];
-
-				// tells what index to splice from columnHeaders
-				let i = 0;
-
-				// fills in timeSlots (all of the data)
-				daysOfWeek.forEach(day => {
-					if (value[day] !== undefined) {
-						if (value[day].length > 0) {
-							value[day].forEach(timeSlot => {
-								const indexInTimeSlots =
-									indexInTimeSlot[timeSlot];
-								timeSlots[indexInTimeSlots][day] = timeSlot;
-							});
-							i++;
-						} else {
-							columnHeaders.splice(i, 1);
-						}
-
-						//deletes all of the unnecessary rows
-						if (day === 'sunday') {
-							for (i = 0; i < timeSlots.length; ) {
-								const row = timeSlots[i];
-								if (Object.keys(row).length > 1) {
-									i++;
-								} else {
-									timeSlots.splice(i, 1);
-								}
-							}
-						}
-					}
-				});
-
-				return (
-					<Table
-						dataSource={timeSlots}
-						columns={columnHeaders}
-						bordered={true}
-						pagination={false}
-						showHeader={true}
-						size="small"
-					/>
-				);
-			}
-		} else if (label === 'Interest(s): ') {
+		} else if (label === "Interest(s): ") {
 			// value = profile.interests
-			let formattedInterests = '';
-			let upperCaseInterest = '';
+			let formattedInterests = "";
+			let upperCaseInterest = "";
 			for (let i = 0; i < value.length; i++) {
 				upperCaseInterest =
 					value[i][0].toUpperCase() + value[i].substring(1);
 				// replaces underscore in two word interests with space
-				upperCaseInterest = upperCaseInterest.replace(/_/g, ' ');
-				const spaceIndex = upperCaseInterest.indexOf(' ');
+				upperCaseInterest = upperCaseInterest.replace(/_/g, " ");
+				const spaceIndex = upperCaseInterest.indexOf(" ");
 				if (spaceIndex !== -1) {
 					const secondWordFirstLetterIndex = spaceIndex + 1;
 					upperCaseInterest =
@@ -294,11 +159,11 @@ class DisplayField extends Component {
 				formattedInterests += upperCaseInterest;
 				// adds comma between interests
 				if (value.length === 2 && i === 0) {
-					formattedInterests += ' & ';
+					formattedInterests += " & ";
 				} else if (i !== value.length - 1) {
-					formattedInterests += ', ';
+					formattedInterests += ", ";
 					if (i === value.length - 2) {
-						formattedInterests += '& ';
+						formattedInterests += "& ";
 					}
 				}
 			}
@@ -310,7 +175,7 @@ class DisplayField extends Component {
 		const { colorTheme, label, value, loggedInState } = this.props;
 
 		let textColor = colorTheme.text2Color;
-		if (loggedInState === 'not_logged_in') {
+		if (loggedInState === "not_logged_in") {
 			textColor = GREY_2;
 		}
 		return (
@@ -346,4 +211,7 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps, null)(DisplayField);
+export default connect(
+	mapStateToProps,
+	null
+)(DisplayField);
--- routes/conversationsRoutes.js
+++ routes/conversationsRoutes.js
@@ -5,210 +5,219 @@ const ConversationCollection = mongoose.model("conversations");
 const UserCollection = mongoose.model("users");
 
 const tellContactsUserIsOnline = async (
-  userConversations,
-  mongoDBUserId,
-  serverSocket,
-  clientSocketId,
-  redis
+	userConversations,
+	mongoDBUserId,
+	serverSocket,
+	clientSocketId,
+	redis
 ) => {
-  // tell all the user's contacts that are already online that the user is online
-  for (let i = 0; i < userConversations.length; i++) {
-    const contactMongoDBId = userConversations[i]["matchId"];
-    redis.get(contactMongoDBId, function(err, reply) {
-      if (reply !== null) {
-        const contactSocketId = reply.toString();
-        // contact is online
-        const newUserSocketInfo = {
-          userId: mongoDBUserId,
-          socketId: clientSocketId
-        };
+	// tell all the user's contacts that are already online that the user is online
+	for (let i = 0; i < userConversations.length; i++) {
+		const contactMongoDBId = userConversations[i]["matchId"];
+		redis.get(contactMongoDBId, function(err, reply) {
+			if (reply !== null) {
+				const contactSocketId = reply.toString();
+				// contact is online
+				const newUserSocketInfo = {
+					userId: mongoDBUserId,
+					socketId: clientSocketId
+				};
 
-        // send message to contact telling them we are also online
-        serverSocket
-          .to(contactSocketId)
-          .emit(
-            "TELL_CONTACT_X:ONE_OF_YOUR_CONTACTS_IS_ONLINE",
-            newUserSocketInfo
-          );
-        // console.log(
-        // 	'updated contactMongoDBId = ' +
-        // 		contactMongoDBId +
-        // 		' with our socketId'
-        // );
-      }
-    });
-  }
+				// send message to contact telling them we are also online
+				serverSocket
+					.to(contactSocketId)
+					.emit(
+						"TELL_CONTACT_X:ONE_OF_YOUR_CONTACTS_IS_ONLINE",
+						newUserSocketInfo
+					);
+				// console.log(
+				// 	'updated contactMongoDBId = ' +
+				// 		contactMongoDBId +
+				// 		' with our socketId'
+				// );
+			}
+		});
+	}
 };
 
 const updateUserConversationsWithOnlineContacts = async (
-  userConversations,
-  redis
+	userConversations,
+	redis
 ) => {
-  // https://stackoverflow.com/questions/29693469/node-js-wait-for-all-redis-queries-to-complete-before-continuing-with-execution
-  await Promise.all(
-    _.map(userConversations, userConversation => {
-      return new Promise(function(resolve, reject) {
-        // need to redo this redis.get because await is not working
-        redis.get(userConversation.matchId, function(err, reply) {
-          if (err) {
-            return reject(err);
-          } else if (reply !== null) {
-            const contactSocketId = reply.toString();
-            // the current contact is online
-            userConversation["isOnline"] = true;
-            userConversation["socketId"] = contactSocketId;
-          } else if (reply === null) {
-            // TODO: remove when we actually figure out how to detect user is offline
-            userConversation["isOnline"] = false;
-            userConversation["socketId"] = null;
-          }
-          resolve();
-        });
-      });
-    })
-  )
-    .then(function() {
-      // console.log('All operations are done');
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
-  return userConversations;
+	// https://stackoverflow.com/questions/29693469/node-js-wait-for-all-redis-queries-to-complete-before-continuing-with-execution
+	await Promise.all(
+		_.map(userConversations, userConversation => {
+			return new Promise(function(resolve, reject) {
+				// TODO: redo this redis.get because await is not working
+				redis.get(userConversation.matchId, function(err, reply) {
+					if (err) {
+						return reject(err);
+					} else if (reply !== null) {
+						const contactSocketId = reply.toString();
+						// the current contact is online
+						userConversation["isOnline"] = true;
+						userConversation["socketId"] = contactSocketId;
+					} else if (reply === null) {
+						// TODO: remove when we actually figure out how to detect user is offline
+						userConversation["isOnline"] = false;
+						userConversation["socketId"] = null;
+					}
+					resolve();
+				});
+			});
+		})
+	)
+		.then(function() {
+			// console.log('All operations are done');
+		})
+		.catch(function(err) {
+			console.log(err);
+		});
+	return userConversations;
 };
 
 module.exports = app => {
-  app.get("/api/conversations", requireLogin, async (request, response) => {
-    const conversation = await ConversationCollection.findOne({
-      _id: request.query.conversationId
-    });
-    response.send(conversation);
-  });
+	app.get("/api/conversations", requireLogin, async (request, response) => {
+		const conversation = await ConversationCollection.findOne({
+			_id: request.query.conversationId
+		});
+		response.send(conversation);
+	});
 
-  app.post(
-    "/api/conversations/user_online",
-    requireLogin,
-    async (request, response) => {
-      const { mongoDBUserId, userConversations, clientSocketId } = request.body;
+	app.post(
+		"/api/conversations/user_online",
+		requireLogin,
+		async (request, response) => {
+			const {
+				mongoDBUserId,
+				userConversations,
+				clientSocketId
+			} = request.body;
 
-      const redis = request.app.get("redis");
+			const redis = request.app.get("redis");
 
-      redis.get(mongoDBUserId, function(err, reply) {
-        if (reply !== null) {
-          const oldClientSocketId = reply.toString();
-          redis.del(oldClientSocketId);
-        }
-      });
+			redis.get(mongoDBUserId, function(err, reply) {
+				if (reply !== null) {
+					const oldClientSocketId = reply.toString();
+					redis.del(oldClientSocketId);
+				}
+			});
 
-      redis.set(mongoDBUserId, clientSocketId);
-      redis.set(clientSocketId, mongoDBUserId);
+			redis.set(mongoDBUserId, clientSocketId);
+			redis.set(clientSocketId, mongoDBUserId);
 
-      const serverSocket = request.app.get("serverSocket");
+			const serverSocket = request.app.get("serverSocket");
 
-      tellContactsUserIsOnline(
-        userConversations,
-        mongoDBUserId,
-        serverSocket,
-        clientSocketId,
-        redis
-      );
-      response.send("added user's serverSocketId to redis");
-    }
-  );
+			tellContactsUserIsOnline(
+				userConversations,
+				mongoDBUserId,
+				serverSocket,
+				clientSocketId,
+				redis
+			);
+			response.send("added user's serverSocketId to redis");
+		}
+	);
 
-  app.put(
-    "/api/conversations/online_contacts",
-    requireLogin,
-    async (request, response) => {
-      const redis = request.app.get("redis");
-      const updatedUserConversations = await updateUserConversationsWithOnlineContacts(
-        request.body,
-        redis
-      );
+	app.put(
+		"/api/conversations/online_contacts",
+		requireLogin,
+		async (request, response) => {
+			const redis = request.app.get("redis");
+			const updatedUserConversations = await updateUserConversationsWithOnlineContacts(
+				request.body,
+				redis
+			);
 
-      response.send(updatedUserConversations);
-    }
-  );
+			response.send(updatedUserConversations);
+		}
+	);
 
-  app.post(
-    "/api/conversations/chat",
-    requireLogin,
-    async (request, response) => {
-      const { conversationId, senderId, message, timeCreated } = request.body;
-      // GOAL = save newMessage into correct conversation document
-      const newMessage = {
-        senderId: senderId,
-        content: message,
-        timeCreated: timeCreated,
-        status: "delivered"
-      };
-      const conversation = await ConversationCollection.findOne(
-        {
-          _id: conversationId
-        },
-        { last50Messages: true }
-      );
-      let last50Messages = conversation.last50Messages;
-      last50Messages.push(newMessage);
-      if (last50Messages.length > 50) {
-        // removes first element from array
-        last50Messages.shift();
-      }
+	app.post(
+		"/api/conversations/chat",
+		requireLogin,
+		async (request, response) => {
+			const {
+				conversationId,
+				senderId,
+				message,
+				timeCreated
+			} = request.body;
+			// GOAL = save newMessage into correct conversation document
+			const newMessage = {
+				senderId: senderId,
+				content: message,
+				timeCreated: timeCreated,
+				status: "delivered"
+			};
+			const conversation = await ConversationCollection.findOne(
+				{
+					_id: conversationId
+				},
+				{ last50Messages: true }
+			);
+			let last50Messages = conversation.last50Messages;
+			last50Messages.push(newMessage);
+			if (last50Messages.length > 50) {
+				// removes first element from array
+				last50Messages.shift();
+			}
 
-      try {
-        await ConversationCollection.updateOne(
-          {
-            _id: conversationId
-          },
-          {
-            $set: {
-              last50Messages: last50Messages
-            }
-          }
-        );
+			try {
+				await ConversationCollection.updateOne(
+					{
+						_id: conversationId
+					},
+					{
+						$set: {
+							last50Messages: last50Messages
+						}
+					}
+				);
 
-        response.send(last50Messages);
-      } catch (error) {
-        response.status(422).send(error);
-      }
-    }
-  );
+				response.send(last50Messages);
+			} catch (error) {
+				response.status(422).send(error);
+			}
+		}
+	);
 
-  app.delete(
-    "/api/conversations/delete",
-    requireLogin,
-    async (request, response) => {
-      const { conversationId, contactMongoDBId } = request.body;
-      const userId = request.user._id;
-      try {
-        // https://stackoverflow.com/questions/16959099/how-to-remove-array-element-in-mongodb
-        // deletes conversation in the conversation collection
-        await ConversationCollection.deleteOne({ _id: conversationId });
-        // deletes conversation from user's conversations
-        await UserCollection.update(
-          { _id: userId },
-          {
-            $pull: {
-              "conversations.userConversations": {
-                conversationId: conversationId
-              }
-            }
-          }
-        );
-        // deletes conversation in contact's conversations
-        await UserCollection.update(
-          { _id: contactMongoDBId },
-          {
-            $pull: {
-              "conversations.userConversations": {
-                conversationId: conversationId
-              }
-            }
-          }
-        );
-        response.send("done");
-      } catch (error) {
-        response.status(422).send(error);
-      }
-    }
-  );
+	app.delete(
+		"/api/conversations/delete",
+		requireLogin,
+		async (request, response) => {
+			const { conversationId, contactMongoDBId } = request.body;
+			const userId = request.user._id;
+			try {
+				// https://stackoverflow.com/questions/16959099/how-to-remove-array-element-in-mongodb
+				// deletes conversation in the conversation collection
+				await ConversationCollection.deleteOne({ _id: conversationId });
+				// deletes conversation from user's conversations
+				await UserCollection.update(
+					{ _id: userId },
+					{
+						$pull: {
+							"conversations.userConversations": {
+								conversationId: conversationId
+							}
+						}
+					}
+				);
+				// deletes conversation in contact's conversations
+				await UserCollection.update(
+					{ _id: contactMongoDBId },
+					{
+						$pull: {
+							"conversations.userConversations": {
+								conversationId: conversationId
+							}
+						}
+					}
+				);
+				response.send("done");
+			} catch (error) {
+				response.status(422).send(error);
+			}
+		}
+	);
 };
