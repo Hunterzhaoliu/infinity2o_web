f26fb43b5a3f9541b60d970b0091f619c6c7050e
Q64 <hunterzhaoliu@gmail.com>
2019-03-27 23:20:05 -0400

    changing the number of interests changes the sign of the rows

--- client/src/containers/profile/edit/timeZone/InputTimeZone.js
+++ client/src/containers/profile/edit/timeZone/InputTimeZone.js
@@ -8,103 +8,95 @@ import ErrorMessage from "../ErrorMessage";
 import "./InputTimeZone.css";
 
 class InputTimeZone extends Component {
-  onChangeTimeZone = e => {
-    this.props.onChangeTimeZone(e);
-  };
+	onChangeTimeZone = e => {
+		this.props.onChangeTimeZone(e);
+	};
 
-  render() {
-    const { colorTheme, label, width, profile } = this.props;
-    document.documentElement.style.setProperty(
-      `--text1Color`,
-      colorTheme.text1Color
-    );
-    document.documentElement.style.setProperty(
-      `--text2Color`,
-      colorTheme.text2Color
-    );
-    document.documentElement.style.setProperty(
-      `--text4Color`,
-      colorTheme.text4Color
-    );
-    document.documentElement.style.setProperty(
-      `--text6Color`,
-      colorTheme.text6Color
-    );
-    document.documentElement.style.setProperty(
-      `--text7Color`,
-      colorTheme.text7Color
-    );
-    document.documentElement.style.setProperty(
-      `--text8Color`,
-      colorTheme.text8Color
-    );
-    return (
-      <div>
-        <Row type="flex" justify="start" align="middle">
-          <Col
-            sm={{ span: 6 }}
-            md={{ span: 6 }}
-            lg={{ span: 6 }}
-            xl={{ span: 4 }}
-          >
-            <h3
-              style={{
-                color: colorTheme.text4Color
-              }}
-            >
-              {label}
-            </h3>
-          </Col>
-          <Col
-            sm={{ span: 16, offset: 1 }}
-            md={{ span: 17, offset: 1 }}
-            lg={{ span: 17, offset: 1 }}
-            xl={{ span: 19, offset: 1 }}
-          >
-            <Cascader
-              style={{
-                width: width,
-                background: colorTheme.text8Color
-              }}
-              value={profile.timeZone}
-              onChange={this.onChangeTimeZone}
-              options={timeZones}
-              expandTrigger="hover"
-              allowClear={false}
-              placeholder=""
-            />
-          </Col>
-        </Row>
-        <ErrorMessage
-          message="Need a time zone instead of a country"
-          hasError={profile.hasTimeZoneError}
-        />
-      </div>
-    );
-  }
+	render() {
+		const { colorTheme, profile } = this.props;
+		document.documentElement.style.setProperty(
+			`--text1Color`,
+			colorTheme.text1Color
+		);
+		document.documentElement.style.setProperty(
+			`--text2Color`,
+			colorTheme.text2Color
+		);
+		document.documentElement.style.setProperty(
+			`--text4Color`,
+			colorTheme.text4Color
+		);
+		document.documentElement.style.setProperty(
+			`--text6Color`,
+			colorTheme.text6Color
+		);
+		document.documentElement.style.setProperty(
+			`--text7Color`,
+			colorTheme.text7Color
+		);
+		document.documentElement.style.setProperty(
+			`--text8Color`,
+			colorTheme.text8Color
+		);
+		return (
+			<div>
+				<Row type="flex" justify="start" align="middle">
+					<Col xl={{ span: 2 }}>
+						<h3
+							style={{
+								color: colorTheme.text4Color
+							}}
+						>
+							Time Zone:
+						</h3>
+					</Col>
+					<Col xl={{ span: 19, offset: 1 }}>
+						<Cascader
+							style={{
+								background: colorTheme.text8Color
+							}}
+							value={profile.timeZone}
+							onChange={this.onChangeTimeZone}
+							options={timeZones}
+							expandTrigger="hover"
+							allowClear={false}
+							placeholder=""
+						/>
+					</Col>
+				</Row>
+				<ErrorMessage
+					message="Need a time zone instead of a country"
+					hasError={profile.hasTimeZoneError}
+				/>
+			</div>
+		);
+	}
 }
 /*
 So we have a state and a UI(with props).
 This function gives the UI the parts of the state it will need to display.
 */
 function mapStateToProps(state) {
-  return {
-    colorTheme: state.colorTheme,
-    profile: state.profile
-  };
+	return {
+		colorTheme: state.colorTheme,
+		profile: state.profile
+	};
 }
 
 function mapDispatchToProps(dispatch) {
-  const profileDispatchers = bindActionCreators(
-    profileActionCreators,
-    dispatch
-  );
+	const profileDispatchers = bindActionCreators(
+		profileActionCreators,
+		dispatch
+	);
 
-  return {
-    onChangeTimeZone: newTimeZone => {
-      profileDispatchers.onChangeTimeZone(newTimeZone);
-    }
-  };
+	return {
+		onChangeTimeZone: newTimeZone => {
+			profileDispatchers.onChangeTimeZone(newTimeZone);
+		}
+	};
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(InputTimeZone);
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(InputTimeZone);
