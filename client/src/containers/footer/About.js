8f06b04148913ebec5701240b3f44e580b009655
Q64 <hunterzhaoliu@gmail.com>
2019-04-30 22:25:03 -0400

    fixed about page and added right padding on interests

--- client/src/containers/footer/About.js
+++ client/src/containers/footer/About.js
@@ -1,83 +1,137 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { GREY_1, GREY_9 } from "../styles/ColorConstants";
+import { GREY_1, GREY_3, GREY_9 } from "../styles/ColorConstants";
 import { Layout, Row, Col } from "antd";
+import MatchCards from "../matches/MatchCards";
 import { Helmet } from "react-helmet";
 import "./about.css";
 
 const { Content } = Layout;
 
 class About extends Component {
-	// renderTeam() {
-	// 	const { teamMembers, colorTheme } = this.props;
-	//
-	// 	return _.map(teamMembers, teamMember => {
-	// 		return (
-	// 			<Col>
-	// 				<h1 style={{ color: colorTheme.text2Color }}>Hi</h1>
-	// 			</Col>
-	// 		);
-	// 	});
-	// }
+  // renderTeam() {
+  // 	const { teamMembers, colorTheme } = this.props;
+  //
+  // 	return _.map(teamMembers, teamMember => {
+  // 		return (
+  // 			<Col>
+  // 				<h1 style={{ color: colorTheme.text2Color }}>Hi</h1>
+  // 			</Col>
+  // 		);
+  // 	});
+  // }
 
-	render() {
-		const { colorTheme, loggedInState } = this.props;
+  render() {
+    const { colorTheme, loggedInState } = this.props;
 
-		let background = colorTheme.backgroundColor;
-		let textColor = colorTheme.text1Color;
-		if (loggedInState === "not_logged_in") {
-			background = GREY_1;
-			textColor = GREY_9;
-		}
+    let background = colorTheme.backgroundColor;
+    let headerColor = colorTheme.text1Color;
+    let paragraphColor = colorTheme.text3Color;
+    if (loggedInState === "not_logged_in") {
+      background = GREY_1;
+      headerColor = GREY_9;
+      paragraphColor = GREY_3;
+    }
 
-		return (
-			<Content
-				style={{
-					textAlign: "center",
-					padding: "120px 0px 0px",
-					background: background
-				}}
-			>
-				<Helmet>
-					<title>About</title>
-				</Helmet>
-				<Row type="flex" justify="center">
-					<Col>
-						<h2 className="about-h2" style={{ color: textColor }}>
-							About
-						</h2>
-					</Col>
-				</Row>
-				<Row
-					style={{ padding: "30px 0px 0px 0px" }}
-					type="flex"
-					justify="center"
-				>
-					<Col>
-						<h4
-							className="about-h4"
-							style={{
-								color: colorTheme.text2Color
-							}}
-						>
-							We believe the future of education can become better
-							by following these principles:
-						</h4>
-					</Col>
-				</Row>
-			</Content>
-		);
-	}
+    const q = {
+      name: "Q Liu",
+      age: 25,
+      totalUserVotes: 62,
+      interests: [
+        "entrepreneurship",
+        "nutrition",
+        "robotics",
+        "mechanical_engineering",
+        "electrical_engineering"
+      ],
+      linkedInPublicProfileUrl: "https://www.linkedin.com/in/letterq/",
+      githubPublicProfileUrl: "https://github.com/quinnliu",
+      imageUrl:
+        "https://lh5.googleusercontent.com/-J_P0Sk0phsE/AAAAAAAAAAI/AAAAAAAAACU/kM-9AVW-rl8/photo.j150"
+    };
+
+    const hunter = {
+      name: "Hunter Liu",
+      age: 18,
+      totalUserVotes: 47,
+      interests: [
+        "artificial_intelligence",
+        "computer_science",
+        "design",
+        "economics",
+        "entrepreneurship"
+      ],
+      linkedInPublicProfileUrl: "https://www.linkedin.com/in/hunterzhaoliu/",
+      githubPublicProfileUrl: "https://github.com/Hunter690",
+      imageUrl:
+        "https://lh4.googleusercontent.com/-_cJdeMMJK7c/AAAAAAAAAAI/AAAAAAAAAhA/7XZmb3NeW9g/photo.j150"
+    };
+
+    return (
+      <Content
+        style={{
+          textAlign: "center",
+          padding: "120px 0px 0px",
+          background: background
+        }}
+      >
+        <Helmet>
+          <title>About</title>
+        </Helmet>
+        <Row type="flex" justify="center">
+          <Col>
+            <h2 className="about-h2" style={{ color: headerColor }}>
+              About
+            </h2>
+          </Col>
+        </Row>
+        <Row
+          style={{ padding: "60px 0px 0px 0px" }}
+          type="flex"
+          justify="center"
+        >
+          <Col>
+            <h4 className="about-h4" style={{ color: headerColor }}>
+              We took 20+ online courses alone and then 3 together before
+              starting infinity2o.
+            </h4>
+          </Col>
+        </Row>
+        <Row
+          style={{ padding: "60px 0px 60px 0px" }}
+          type="flex"
+          justify="center"
+        >
+          <Col xl={{ span: 7 }}>
+            <MatchCards match={q} />
+          </Col>
+          <Col xl={{ offset: 1, span: 7 }}>
+            <MatchCards match={hunter} />
+          </Col>
+        </Row>
+        <Row style={{ padding: "0px 0px 60px 0px" }} type="flex" justify="left">
+          <Col xl={{ offset: 5, span: 14 }}>
+            <p className="about-p" style={{ color: paragraphColor }}>
+              &emsp; We believe online courses will be very important in future
+              education. But until now, online courses have been indepedent
+              places to learn reliant on random discussion forums with broken
+              responses rather than real discussion.
+            </p>
+          </Col>
+        </Row>
+      </Content>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		colorTheme: state.colorTheme,
-		loggedInState: state.auth.loggedInState
-	};
+  return {
+    colorTheme: state.colorTheme,
+    loggedInState: state.auth.loggedInState
+  };
 }
 
 export default connect(
-	mapStateToProps,
-	null
+  mapStateToProps,
+  null
 )(About);
--- client/src/containers/profileInformation/Interests.js
+++ client/src/containers/profileInformation/Interests.js
@@ -4,81 +4,79 @@ import { Row, Col } from "antd";
 import interestIcon from "../images/interestIcon.png";
 
 class Interests extends Component {
-	renderInterests() {
-		const { interests } = this.props;
-		// interests = profile.interests
-		let formattedInterests = "";
-		let upperCaseInterest = "";
-		for (let i = 0; i < interests.length; i++) {
-			upperCaseInterest =
-				interests[i][0].toUpperCase() + interests[i].substring(1);
-			// replaces underscore in two word interests with space
-			upperCaseInterest = upperCaseInterest.replace(/_/g, " ");
-			const spaceIndex = upperCaseInterest.indexOf(" ");
-			if (spaceIndex !== -1) {
-				const secondWordFirstLetterIndex = spaceIndex + 1;
-				upperCaseInterest =
-					upperCaseInterest.substr(0, secondWordFirstLetterIndex) +
-					upperCaseInterest[
-						secondWordFirstLetterIndex
-					].toUpperCase() +
-					upperCaseInterest.substr(secondWordFirstLetterIndex + 1);
-			}
+  renderInterests() {
+    const { interests } = this.props;
+    // interests = profile.interests
+    let formattedInterests = "";
+    let upperCaseInterest = "";
+    for (let i = 0; i < interests.length; i++) {
+      upperCaseInterest =
+        interests[i][0].toUpperCase() + interests[i].substring(1);
+      // replaces underscore in two word interests with space
+      upperCaseInterest = upperCaseInterest.replace(/_/g, " ");
+      const spaceIndex = upperCaseInterest.indexOf(" ");
+      if (spaceIndex !== -1) {
+        const secondWordFirstLetterIndex = spaceIndex + 1;
+        upperCaseInterest =
+          upperCaseInterest.substr(0, secondWordFirstLetterIndex) +
+          upperCaseInterest[secondWordFirstLetterIndex].toUpperCase() +
+          upperCaseInterest.substr(secondWordFirstLetterIndex + 1);
+      }
 
-			formattedInterests += upperCaseInterest;
-			// adds comma between interests
-			if (interests.length === 2 && i === 0) {
-				formattedInterests += " & ";
-			} else if (i !== interests.length - 1) {
-				formattedInterests += ", ";
-				if (i === interests.length - 2) {
-					formattedInterests += "& ";
-				}
-			}
-		}
-		return formattedInterests;
-	}
+      formattedInterests += upperCaseInterest;
+      // adds comma between interests
+      if (interests.length === 2 && i === 0) {
+        formattedInterests += " & ";
+      } else if (i !== interests.length - 1) {
+        formattedInterests += ", ";
+        if (i === interests.length - 2) {
+          formattedInterests += "& ";
+        }
+      }
+    }
+    return formattedInterests;
+  }
 
-	render() {
-		const { interests, textColor } = this.props;
-		if (interests !== null && interests.length > 0) {
-			return (
-				<Row
-					style={{ padding: "15px 0px 0px 0px" }}
-					type="flex"
-					justify="start"
-					align="middle"
-				>
-					<Col span={1}>
-						<img
-							alt="Interests: "
-							style={{
-								width: "25px"
-							}}
-							src={interestIcon}
-						/>
-					</Col>
-					<Col
-						span={23}
-						style={{
-							padding: "0px 0px 0px 20px",
-							fontFamily: "Overpass",
-							lineHeight: 1.2,
-							fontSize: 16,
-							color: textColor
-						}}
-					>
-						{this.renderInterests()}
-					</Col>
-				</Row>
-			);
-		} else {
-			return <div />;
-		}
-	}
+  render() {
+    const { interests, textColor } = this.props;
+    if (interests !== null && interests.length > 0) {
+      return (
+        <Row
+          style={{ padding: "15px 0px 0px 0px" }}
+          type="flex"
+          justify="start"
+          align="middle"
+        >
+          <Col span={1}>
+            <img
+              alt="Interests: "
+              style={{
+                width: "25px"
+              }}
+              src={interestIcon}
+            />
+          </Col>
+          <Col
+            span={23}
+            style={{
+              padding: "0px 10px 0px 20px",
+              fontFamily: "Overpass",
+              lineHeight: 1.2,
+              fontSize: 16,
+              color: textColor
+            }}
+          >
+            {this.renderInterests()}
+          </Col>
+        </Row>
+      );
+    } else {
+      return <div />;
+    }
+  }
 }
 
 export default connect(
-	null,
-	null
+  null,
+  null
 )(Interests);
