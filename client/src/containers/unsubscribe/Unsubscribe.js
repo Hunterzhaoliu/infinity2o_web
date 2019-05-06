9a8541fe524c973c7036a9bb144be8fdb6f05057
Q Liu <emn1over12@gmail.com>
2019-05-05 21:26:20 -0400

    Y

--- client/src/containers/unsubscribe/Unsubscribe.js
+++ client/src/containers/unsubscribe/Unsubscribe.js
@@ -12,132 +12,132 @@ import { Helmet } from "react-helmet";
 const { Content } = Layout;
 
 class Unsubscribe extends Component {
-	componentWillMount() {
-		// run once before first render()
-		const URLArray = window.location.href.split("/");
-		// URLArray =  [ "http:", "", "localhost:3000", "unsubscribe", "5ad55047b4e23300148b0cd7" ]
-		const userId = URLArray[4];
-		this.props.userUnsubscribed(userId);
-	}
+  componentWillMount() {
+    // run once before first render()
+    const URLArray = window.location.href.split("/");
+    // URLArray =  [ "http:", "", "localhost:3000", "unsubscribe", "5ad55047b4e23300148b0cd7" ]
+    const userId = URLArray[4];
+    this.props.userUnsubscribed(userId);
+  }
 
-	renderSeeMatchesButton(loggedInState) {
-		if (loggedInState === "logged_in") {
-			return <MatchesButton />;
-		} else {
-			return <LoginButtons />;
-		}
-	}
-	render() {
-		const { name, loggedInState, colorTheme } = this.props;
-		const userFirstName = name.split(" ")[0];
-		let backgroundColor = GREY_1;
-		let cardColor = GREY_DOT_5;
-		let textColor = GREY_7;
-		if (loggedInState === "logged_in") {
-			backgroundColor = colorTheme.backgroundColor;
-			cardColor = colorTheme.text9Dot5Color;
-			textColor = colorTheme.text1Color;
-		}
+  renderSeeMatchesButton(loggedInState) {
+    if (loggedInState === "logged_in") {
+      return <MatchesButton />;
+    } else {
+      return <LoginButtons />;
+    }
+  }
 
-		const greeting = "Hey " + userFirstName + ",";
-		const thankfulNote = "thanks for being a part of ";
+  render() {
+    const { name, loggedInState, colorTheme } = this.props;
+    const userFirstName = name.split(" ")[0];
+    let backgroundColor = GREY_1;
+    let cardColor = GREY_DOT_5;
+    let textColor = GREY_7;
+    if (loggedInState === "logged_in") {
+      backgroundColor = colorTheme.backgroundColor;
+      cardColor = colorTheme.text9Dot5Color;
+      textColor = colorTheme.text1Color;
+    }
 
-		return (
-			<Content
-				style={{
-					textAlign: "center",
-					padding: "120px 0px 0px 0px",
-					background: backgroundColor
-				}}
-			>
-				<Helmet>
-					<title>Unsubscribe</title>
-				</Helmet>
-				<Row type="flex" justify="center" align="top">
-					<Col>
-						<Card
-							loading={false}
-							style={{
-								color: textColor,
-								background: cardColor
-							}}
-							bodyStyle={{ padding: "60px" }} // padding around inside border of card
-						>
-							<Row type="flex" justify="center" align="middle">
-								<Col>
-									<h2
-										style={{
-											fontSize: "30px",
-											fontFamily: "Overpass",
-											lineHeight: 1,
-											marginBottom: 0
-										}}
-									>
-										{greeting}
-									</h2>
-								</Col>
-							</Row>
-							<Row
-								style={{ padding: "15px 0px 20px" }}
-								type="flex"
-								justify="center"
-								align="middle"
-							>
-								<Col>
-									<h4
-										style={{
-											fontSize: "24px",
-											fontFamily: "Overpass",
-											lineHeight: 1,
-											marginBottom: 0,
-											padding: "0px 10px 0px 0px"
-										}}
-									>
-										{thankfulNote}
-									</h4>
-								</Col>
-								<Col>
-									<img alt="" src={logo} width="50px" />
-								</Col>
-							</Row>
-							<Row type="flex" justify="center" align="middle">
-								<Col>
-									<hr
-										style={{
-											border: "none",
-											backgroundColor: backgroundColor,
-											height: "2px"
-										}}
-										width="250px"
-									/>
-								</Col>
-							</Row>
-							<Row
-								type="flex"
-								justify="center"
-								align="middle"
-								style={{ padding: "30px 0px 0px" }}
-							>
-								<Col>
-									<p
-										style={{
-											fontSize: "18px",
-											fontFamily: "Overpass",
-											marginBottom: 0
-										}}
-									>
-										&#123; Insert super long reason why you
-										should stay <br />
-										subscribed to our email service &#125;
-									</p>
-								</Col>
-							</Row>
-						</Card>
-					</Col>
-				</Row>
-			</Content>
-		);
-	}
+    const greeting = "Hey " + userFirstName + ",";
+    const thankfulNote = "thanks for being a part of ";
+
+    return (
+      <Content
+        style={{
+          textAlign: "center",
+          padding: "120px 0px 0px 0px",
+          background: backgroundColor
+        }}
+      >
+        <Helmet>
+          <title>Unsubscribe</title>
+        </Helmet>
+        <Row type="flex" justify="center" align="top">
+          <Col>
+            <Card
+              loading={false}
+              style={{
+                color: textColor,
+                background: cardColor
+              }}
+              bodyStyle={{ padding: "60px" }} // padding around inside border of card
+            >
+              <Row type="flex" justify="center" align="middle">
+                <Col>
+                  <h2
+                    style={{
+                      fontSize: "30px",
+                      fontFamily: "Overpass",
+                      lineHeight: 1,
+                      marginBottom: 0
+                    }}
+                  >
+                    {greeting}
+                  </h2>
+                </Col>
+              </Row>
+              <Row
+                style={{ padding: "15px 0px 20px" }}
+                type="flex"
+                justify="center"
+                align="middle"
+              >
+                <Col>
+                  <h4
+                    style={{
+                      fontSize: "24px",
+                      fontFamily: "Overpass",
+                      lineHeight: 1,
+                      marginBottom: 0,
+                      padding: "0px 10px 0px 0px"
+                    }}
+                  >
+                    {thankfulNote}
+                  </h4>
+                </Col>
+                <Col>
+                  <img alt="" src={logo} width="50px" />
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" align="middle">
+                <Col>
+                  <hr
+                    style={{
+                      border: "none",
+                      backgroundColor: backgroundColor,
+                      height: "2px"
+                    }}
+                    width="250px"
+                  />
+                </Col>
+              </Row>
+              <Row
+                type="flex"
+                justify="center"
+                align="middle"
+                style={{ padding: "30px 0px 0px" }}
+              >
+                <Col>
+                  <p
+                    style={{
+                      fontSize: "18px",
+                      fontFamily: "Overpass",
+                      marginBottom: 0
+                    }}
+                  >
+                    &#123; Insert super long reason why you should stay <br />
+                    subscribed to our email service &#125;
+                  </p>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
+      </Content>
+    );
+  }
 }
 
 /*
@@ -145,11 +145,11 @@ So we have a state and a UI(with props).
 This function gives the UI the parts of the state it will need to display.
 */
 function mapStateToProps(state) {
-	return {
-		colorTheme: state.colorTheme,
-		name: state.profile.name,
-		loggedInState: state.auth.loggedInState
-	};
+  return {
+    colorTheme: state.colorTheme,
+    name: state.profile.name,
+    loggedInState: state.auth.loggedInState
+  };
 }
 
 /*
@@ -157,19 +157,19 @@ So we have a state and a UI(with props).
 This function gives the UI the functions it will need to be called.
 */
 function mapDispatchToProps(dispatch) {
-	const unsubscribeDispatchers = bindActionCreators(
-		unsubscribeActionCreators,
-		dispatch
-	);
+  const unsubscribeDispatchers = bindActionCreators(
+    unsubscribeActionCreators,
+    dispatch
+  );
 
-	return {
-		userUnsubscribed: userId => {
-			unsubscribeDispatchers.userUnsubscribed(userId);
-		}
-	};
+  return {
+    userUnsubscribed: userId => {
+      unsubscribeDispatchers.userUnsubscribed(userId);
+    }
+  };
 }
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Unsubscribe);
