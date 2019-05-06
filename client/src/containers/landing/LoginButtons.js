59ac05aec48122fbff839f43d4b3678215c52257
Q Liu <emn1over12@gmail.com>
2019-05-05 23:06:23 -0400

    Y

--- client/src/containers/landing/LoginButtons.js
+++ client/src/containers/landing/LoginButtons.js
@@ -5,64 +5,64 @@ import { Row, Col, Icon } from "antd";
 import "./login-buttons.css";
 
 class LoginButtons extends Component {
-	render() {
-		const { windowWidth } = this.props;
+  render() {
+    const { windowWidth } = this.props;
 
-		document.documentElement.style.setProperty(`--GREY_1`, GREY_1);
+    document.documentElement.style.setProperty(`--GREY_1`, GREY_1);
 
-		let largeGmailLoginText = "Google Login ";
-		let largeLinkedInLoginText = "LinkedIn Login ";
-		let smallLoginText = "";
+    let largeGmailLoginText = "Gmail Login ";
+    let largeLinkedInLoginText = "LinkedIn Login ";
+    let smallLoginText = "";
 
-		if (windowWidth < 768) {
-			// less than medium screen, need to change where the infinity2o logo
-			// is and adjust text size
-			largeGmailLoginText = "";
-			largeLinkedInLoginText = "";
-			smallLoginText = " Login";
-		}
+    if (windowWidth < 768) {
+      // less than medium screen, need to change where the infinity2o logo
+      // is and adjust text size
+      largeGmailLoginText = "";
+      largeLinkedInLoginText = "";
+      smallLoginText = " Login";
+    }
 
-		return (
-			<Col>
-				<Row type="flex" justify="start" align="middle">
-					<Col>
-						<a
-							style={{
-								borderColor: RED_ORANGE_7,
-								background: RED_ORANGE_7
-							}}
-							className="login-anchor"
-							href="/auth/google"
-						>
-							{largeGmailLoginText}
-							<Icon style={{ fontSize: 18 }} type="google" />
-							{smallLoginText}
-						</a>
-					</Col>
-					<Col style={{ padding: "0px 0px 0px 20px" }}>
-						<a
-							style={{ borderColor: BLUE_7, background: BLUE_7 }}
-							className="login-anchor"
-							href="/auth/linkedIn"
-						>
-							{largeLinkedInLoginText}
-							<Icon style={{ fontSize: 18 }} type="linkedin" />
-							{smallLoginText}
-						</a>
-					</Col>
-				</Row>
-			</Col>
-		);
-	}
+    return (
+      <Col>
+        <Row type="flex" justify="start" align="middle">
+          <Col>
+            <a
+              style={{
+                borderColor: RED_ORANGE_7,
+                background: RED_ORANGE_7
+              }}
+              className="login-anchor"
+              href="/auth/google"
+            >
+              {largeGmailLoginText}
+              <Icon style={{ fontSize: 18 }} type="google" />
+              {smallLoginText}
+            </a>
+          </Col>
+          <Col style={{ padding: "0px 0px 0px 20px" }}>
+            <a
+              style={{ borderColor: BLUE_7, background: BLUE_7 }}
+              className="login-anchor"
+              href="/auth/linkedIn"
+            >
+              {largeLinkedInLoginText}
+              <Icon style={{ fontSize: 18 }} type="linkedin" />
+              {smallLoginText}
+            </a>
+          </Col>
+        </Row>
+      </Col>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		windowWidth: state.customHeader.windowWidth
-	};
+  return {
+    windowWidth: state.customHeader.windowWidth
+  };
 }
 
 export default connect(
-	mapStateToProps,
-	null
+  mapStateToProps,
+  null
 )(LoginButtons);
--- client/src/containers/landing/Marketing.js
+++ client/src/containers/landing/Marketing.js
@@ -2,170 +2,178 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { GREY_2, GREY_1 } from "../styles/ColorConstants";
 import { Row, Col } from "antd";
-import onlineClassAlone from "../images/onlineClassAlone.png";
+
+import onlineClassBoy from "../images/onlineClassBoy.png";
+import onlineClassGirl from "../images/onlineClassGirl.png";
 import futureOnlineClasses from "../images/futureOnlineClasses.png";
 
 class Marketing extends Component {
-	renderCartoons() {
-		const { windowWidth } = this.props;
+  renderCartoons() {
+    const { windowWidth } = this.props;
 
-		let h3Padding = "60px 0px 0px 0px";
-		let imagePadding = "30px 0px 0px 0px";
-		let h3FontSize = 28;
-		let imageHeight = "300px";
-		if (windowWidth < 768) {
-			h3Padding = "30px 0px 0px 0px";
-			imagePadding = "15px 0px 0px 0px";
-			h3FontSize = 18;
-			imageHeight = "150px";
-		}
+    let h3Padding = "60px 0px 0px 0px";
+    let imagePadding = "30px 0px 0px 0px";
+    let h3FontSize = 28;
+    let imageHeight = "280px";
+    if (windowWidth < 768) {
+      h3Padding = "30px 0px 0px 0px";
+      imagePadding = "15px 0px 0px 0px";
+      h3FontSize = 18;
+      imageHeight = "150px";
+    }
 
-		return (
-			<div>
-				<Row type="flex" justify="left">
-					<Col
-						style={{
-							padding: h3Padding
-						}}
-						xs={{ span: 24 }}
-						sm={{ span: 11 }}
-						md={{ span: 10 }}
-						lg={{ span: 11 }}
-						xl={{ offset: 3, span: 9 }} // half of the screen
-					>
-						<Row type="flex" justify="center">
-							<Col>
-								<h3
-									style={{
-										color: GREY_2,
-										fontFamily: "Overpass",
-										fontWeight: "bold",
-										fontSize: h3FontSize,
-										marginBottom: 0,
-										lineHeight: 1
-									}}
-								>
-									Online class alone
-								</h3>
-							</Col>
-						</Row>
-						<Row
-							style={{
-								padding: imagePadding
-							}}
-							type="flex"
-							justify="center"
-						>
-							<Col>
-								<img
-									alt=""
-									style={{
-										height: imageHeight
-									}}
-									src={onlineClassAlone}
-								/>
-							</Col>
-						</Row>
-					</Col>
-					<Col
-						style={{
-							padding: h3Padding
-						}}
-						xs={{ span: 24 }}
-						sm={{ span: 11 }}
-						md={{ span: 12 }}
-						lg={{ span: 11 }}
-						xl={{ span: 7 }}
-					>
-						<Row type="flex" justify="center">
-							<Col>
-								<h3
-									style={{
-										textAlign: "center",
-										color: GREY_2,
-										fontFamily: "Overpass",
-										fontWeight: "bold",
-										fontSize: h3FontSize,
-										marginBottom: 0,
-										lineHeight: 1
-									}}
-								>
-									Online class through infinity2o
-								</h3>
-							</Col>
-						</Row>
-						<Row
-							style={{
-								padding: imagePadding
-							}}
-							type="flex"
-							justify="center"
-						>
-							<Col>
-								<img
-									alt=""
-									style={{
-										height: imageHeight
-									}}
-									src={futureOnlineClasses}
-								/>
-							</Col>
-						</Row>
-					</Col>
-				</Row>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <Row type="flex" justify="left">
+          <Col
+            style={{
+              padding: h3Padding
+            }}
+            xs={{ span: 24 }}
+            sm={{ span: 11 }}
+            md={{ span: 10 }}
+            lg={{ span: 11 }}
+            xl={{ offset: 3, span: 9 }} // half of the screen
+          >
+            <Row type="flex" justify="center">
+              <Col>
+                <h3
+                  style={{
+                    color: GREY_2,
+                    fontFamily: "Overpass",
+                    fontWeight: "bold",
+                    fontSize: h3FontSize,
+                    marginBottom: 0,
+                    lineHeight: 1
+                  }}
+                >
+                  Online course alone
+                </h3>
+              </Col>
+            </Row>
+            <Row
+              style={{
+                padding: imagePadding
+              }}
+              type="flex"
+              justify="center"
+            >
+              <Col>
+                <img
+                  alt=""
+                  style={{
+                    height: imageHeight
+                  }}
+                  src={onlineClassBoy}
+                />
+              </Col>
+              <Col>
+                <img
+                  alt=""
+                  style={{
+                    height: imageHeight,
+                    padding: "0px 0px 0px 42px" // TLBR
+                  }}
+                  src={onlineClassGirl}
+                />
+              </Col>
+            </Row>
+          </Col>
+          <Col
+            style={{
+              padding: h3Padding
+            }}
+            xs={{ span: 24 }}
+            sm={{ span: 11 }}
+            md={{ span: 12 }}
+            lg={{ span: 11 }}
+            xl={{ span: 7 }}
+          >
+            <Row type="flex" justify="center">
+              <Col>
+                <h3
+                  style={{
+                    textAlign: "center",
+                    color: GREY_2,
+                    fontFamily: "Overpass",
+                    fontWeight: "bold",
+                    fontSize: h3FontSize,
+                    marginBottom: 0,
+                    lineHeight: 1
+                  }}
+                >
+                  Online course through infinity2o
+                </h3>
+              </Col>
+            </Row>
+            <Row
+              style={{
+                padding: imagePadding
+              }}
+              type="flex"
+              justify="center"
+            >
+              <Col>
+                <img
+                  alt=""
+                  style={{
+                    height: imageHeight
+                  }}
+                  src={futureOnlineClasses}
+                />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 
-	render() {
-		const { windowWidth } = this.props;
+  render() {
+    const { windowWidth } = this.props;
 
-		let h1Padding = "180px 0px 0px 0px";
-		let endColumnHeight = "120px";
-		let h1FontSize = 56;
-		if (windowWidth < 768) {
-			h1Padding = "120px 0px 0px 0px";
-			h1FontSize = 24;
-			endColumnHeight = "60px";
-		}
+    let h1Padding = "180px 0px 0px 0px";
+    let endColumnHeight = "120px";
+    let h1FontSize = 56;
+    if (windowWidth < 768) {
+      h1Padding = "120px 0px 0px 0px";
+      h1FontSize = 24;
+      endColumnHeight = "60px";
+    }
 
-		return (
-			<div>
-				<Row
-					type="flex"
-					justify="center"
-					style={{ padding: h1Padding }}
-				>
-					<Col
-						xs={{ span: 21 }}
-						sm={{ span: 21 }}
-						md={{ span: 22 }}
-						lg={{ span: 23 }}
-						xl={{ span: 24 }}
-					>
-						<h1
-							key="0"
-							style={{
-								textAlign: "center",
-								color: GREY_1,
-								fontSize: h1FontSize,
-								fontFamily: "Overpass",
-								fontWeight: "bold",
-								marginBottom: 0,
-								lineHeight: 1
-							}}
-						>
-							Meet your online course partner.
-						</h1>
-					</Col>
-				</Row>
-				{this.renderCartoons()}
-				<Row>
-					<Col style={{ height: endColumnHeight }} />
-				</Row>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <Row type="flex" justify="center" style={{ padding: h1Padding }}>
+          <Col
+            xs={{ span: 21 }}
+            sm={{ span: 21 }}
+            md={{ span: 22 }}
+            lg={{ span: 23 }}
+            xl={{ span: 24 }}
+          >
+            <h1
+              key="0"
+              style={{
+                textAlign: "center",
+                color: GREY_1,
+                fontSize: h1FontSize,
+                fontFamily: "Overpass",
+                fontWeight: "bold",
+                marginBottom: 0,
+                lineHeight: 1
+              }}
+            >
+              Meet your online course partner.
+            </h1>
+          </Col>
+        </Row>
+        {this.renderCartoons()}
+        <Row>
+          <Col style={{ height: endColumnHeight }} />
+        </Row>
+      </div>
+    );
+  }
 }
 
 /*
@@ -173,12 +181,12 @@ So we have a state and a UI(with props).
 This function gives the UI the parts of the state it will need to display.
 */
 function mapStateToProps(state) {
-	return {
-		windowWidth: state.customHeader.windowWidth
-	};
+  return {
+    windowWidth: state.customHeader.windowWidth
+  };
 }
 
 export default connect(
-	mapStateToProps,
-	null
+  mapStateToProps,
+  null
 )(Marketing);
