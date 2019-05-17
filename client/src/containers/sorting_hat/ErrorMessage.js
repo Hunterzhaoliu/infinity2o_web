a66e8a4b0912c5c78a9f17c09b3b0d71dba0f72e
Hunter Liu <hunterzhaoliu@gmail.com>
2019-05-17 14:18:54 -0400

    made asking a question on sorting hat page screen size friendly

--- client/src/containers/sorting_hat/ErrorMessage.js
+++ client/src/containers/sorting_hat/ErrorMessage.js
@@ -3,31 +3,40 @@ import { connect } from "react-redux";
 import { Row, Col } from "antd";
 
 class ErrorMessage extends Component {
-	render() {
-		const { colorTheme, message, hasError } = this.props;
-		if (hasError) {
-			return (
-				<Row
-					style={{ padding: "5px 0px 0px 0px" }}
-					type="flex"
-					justify="start"
-					align="middle"
-				>
-					<Col xl={{ offset: 4 }}>
-						<div
-							style={{
-								color: colorTheme.text3Color
-							}}
-						>
-							{message}
-						</div>
-					</Col>
-				</Row>
-			);
-		} else {
-			return <div />;
-		}
-	}
+  render() {
+    const { colorTheme, message, hasError } = this.props;
+    if (hasError) {
+      return (
+        <Row
+          style={{ padding: "5px 0px 0px 0px" }}
+          type="flex"
+          justify="start"
+          align="middle"
+        >
+          <Col
+            xs={{ offset: 0 }}
+            sm={{ offset: 0 }}
+            md={{ offset: 0 }}
+            lg={{ offset: 0 }}
+            xl={{ offset: 3 }}
+          >
+            <p
+              style={{
+                color: colorTheme.text3Color,
+                fontFamily: "Overpass",
+                lineHeight: 1,
+                marginBottom: 0
+              }}
+            >
+              {message}
+            </p>
+          </Col>
+        </Row>
+      );
+    } else {
+      return <div />;
+    }
+  }
 }
 
 /*
@@ -35,12 +44,12 @@ So we have a state and a UI(with props).
 This function gives the UI the parts of the state it will need to display.
 */
 function mapStateToProps(state) {
-	return {
-		colorTheme: state.colorTheme
-	};
+  return {
+    colorTheme: state.colorTheme
+  };
 }
 
 export default connect(
-	mapStateToProps,
-	null
+  mapStateToProps,
+  null
 )(ErrorMessage);
